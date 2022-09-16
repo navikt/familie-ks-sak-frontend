2045a5275d57072506441c485bd000c1997b980d
bragejahren <brage.ekroll.jahren@nav.no>
2022-09-16 14:43:52 +0200

    Fikset bygg

--- src/frontend/utils/test/vilkårsvurdering/vilkår.mock.ts
+++ src/frontend/utils/test/vilkårsvurdering/vilkår.mock.ts
@@ -1,66 +1,10 @@
-import { Valideringsstatus } from '@navikt/familie-skjema';
-import type { FeltState } from '@navikt/familie-skjema';
-
-import type { VedtakBegrunnelse } from '../../../typer/vedtak';
 import type {
     IRestAnnenVurdering,
     IRestPersonResultat,
     IRestVilkårResultat,
-    IVilkårResultat,
-    UtdypendeVilkårsvurdering,
 } from '../../../typer/vilkår';
 import { Regelverk, Resultat, VilkårType } from '../../../typer/vilkår';
-import type { IPeriode } from '../../kalender';
 import { erIkkeGenereltVilkår } from '../../vilkår';
-
-interface IMockVilkårResultat {
-    behandlingId?: number;
-    id?: number;
-    resultat?: Resultat;
-    vilkårType?: VilkårType;
-    periode?: IPeriode;
-    begrunnelse?: string;
-    endretAv?: string;
-    erVurdert?: boolean;
-    erAutomatiskVurdert?: boolean;
-    utdypendeVilkårsvurderinger?: UtdypendeVilkårsvurdering[];
-    endretTidspunkt?: string;
-}
-
-export const mockFeltstate = <T>(verdi: T): FeltState<T> => ({
-    feilmelding: '',
-    valider: (feltState, _) => feltState,
-    valideringsstatus: Valideringsstatus.OK,
-    verdi,
-});
-
-export const mockVilkårResultater = ({
-    id = 1,
-    resultat = Resultat.OPPFYLT,
-    behandlingId = 1,
-    vilkårType = VilkårType.LOVLIG_OPPHOLD,
-    periode = { fom: '2000-01-01' },
-    begrunnelse = '',
-    endretAv = 'VL',
-    erVurdert = false,
-    erAutomatiskVurdert = false,
-    utdypendeVilkårsvurderinger,
-    endretTidspunkt = '2020-03-19T09:08:56.8',
-}: IMockVilkårResultat = {}): IVilkårResultat => ({
-    id,
-    vilkårType,
-    resultat: mockFeltstate(resultat),
-    periode: mockFeltstate<IPeriode>(periode),
-    begrunnelse: mockFeltstate(begrunnelse),
-    endretAv,
-    erVurdert,
-    erAutomatiskVurdert,
-    endretTidspunkt,
-    behandlingId,
-    avslagBegrunnelser: mockFeltstate<VedtakBegrunnelse[]>([]),
-    vurderesEtter: Regelverk.NASJONALE_REGLER,
-    utdypendeVilkårsvurderinger: mockFeltstate(utdypendeVilkårsvurderinger ?? []),
-});
 
 interface IMockRestPersonResultat {
     personIdent?: string;
@@ -99,7 +43,7 @@ export const mockRestVilkårResultat = ({
     endretTidspunkt: '2020-03-19T09:08:56.8',
     behandlingId,
     avslagBegrunnelser: [],
-    vurderesEtter: erIkkeGenereltVilkår(vilkårType) ? vurderesEtter : null,
+    vurderesEtter: erIkkeGenereltVilkår(vilkårType) ? vurderesEtter : undefined,
     utdypendeVilkårsvurderinger: [],
 });
 
