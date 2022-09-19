2f735d0e73309fb52a840d827296a1b0078a469d
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2022-09-19 22:16:26 +0200

    Oppdater gruppeIdTilRolle

--- src/frontend/context/AppContext.tsx
+++ src/frontend/context/AppContext.tsx
@@ -7,7 +7,7 @@ import createUseContext from 'constate';
 import { Knapp } from 'nav-frontend-knapper';
 import { Normaltekst } from 'nav-frontend-typografi';
 
-import { HttpProvider, useHttp } from '@navikt/familie-http';
+import { HttpProvider, useHttp, loggFeil } from '@navikt/familie-http';
 import { RessursStatus } from '@navikt/familie-typer';
 import type { ISaksbehandler } from '@navikt/familie-typer';
 import type { Ressurs } from '@navikt/familie-typer';
@@ -21,6 +21,7 @@ import { adressebeskyttelsestyper } from '../typer/person';
 import type { IToggles } from '../typer/toggles';
 import { alleTogglerAv, ToggleNavn } from '../typer/toggles';
 import { gruppeIdTilRolle } from '../utils/behandling';
+import { tilFeilside } from '../utils/commons';
 
 const FEM_MINUTTER = 300000;
 
@@ -218,14 +219,14 @@ const [AppContentProvider, useApp] = createUseContext(() => {
             });
         }
 
-        // if (innloggetSaksbehandler && rolle === BehandlerRolle.UKJENT) {
-        //     loggFeil(
-        //         undefined,
-        //         innloggetSaksbehandler,
-        //         'Saksbehandler tilhører ingen av de definerte tilgangsgruppene.'
-        //     );
-        //     tilFeilside();
-        // }
+        if (innloggetSaksbehandler && rolle === BehandlerRolle.UKJENT) {
+            loggFeil(
+                undefined,
+                innloggetSaksbehandler,
+                'Saksbehandler tilhører ingen av de definerte tilgangsgruppene.'
+            );
+            tilFeilside();
+        }
 
         return rolle;
     };
