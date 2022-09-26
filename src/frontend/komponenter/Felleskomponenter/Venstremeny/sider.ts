88d173d77e905045d1e46a93e4ee1379d6d4dfb6
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2022-09-26 07:59:28 +0200

    Tea-9714: Fjerner unødvendige sjekk på felter vi ikke lenger har

--- src/frontend/komponenter/Felleskomponenter/Venstremeny/sider.ts
+++ src/frontend/komponenter/Felleskomponenter/Venstremeny/sider.ts
@@ -115,9 +115,6 @@ export const sider: Record<SideId, ISide> = {
         href: 'simulering',
         navn: 'Simulering',
         steg: BehandlingSteg.VURDER_TILBAKEKREVING,
-        visSide: (åpenBehandling: IBehandling) => {
-            return !åpenBehandling.skalBehandlesAutomatisk;
-        },
     },
     VEDTAK: {
         href: 'vedtak',
