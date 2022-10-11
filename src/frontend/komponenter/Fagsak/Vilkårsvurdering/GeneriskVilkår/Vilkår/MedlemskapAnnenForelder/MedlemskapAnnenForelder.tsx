d77992143f1f190dd45b792cc1d68e5085cd0521
bragejahren <70642183+bragejahren@users.noreply.github.com>
2022-10-11 08:59:08 +0200

    Justert vilkåret: MedlemskapAnnenForelder
    * Lagt til 'Ikke aktuelt' som alternativ og justert validering

--- src/frontend/komponenter/Fagsak/Vilkårsvurdering/GeneriskVilkår/Vilkår/MedlemskapAnnenForelder/MedlemskapAnnenForelder.tsx
+++ src/frontend/komponenter/Fagsak/Vilkårsvurdering/GeneriskVilkår/Vilkår/MedlemskapAnnenForelder/MedlemskapAnnenForelder.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import { Radio } from 'nav-frontend-skjema';
+
+import { FamilieRadioGruppe } from '@navikt/familie-form-elements';
+
+import { Resultat } from '../../../../../../typer/vilkår';
 import type { IVilkårSkjemaBaseProps } from '../../VilkårSkjema';
 import { VilkårSkjema } from '../../VilkårSkjema';
 import { useVilkårSkjema } from '../../VilkårSkjemaContext';
@@ -20,12 +25,57 @@ export const MedlemskapAnnenForelder: React.FC<MedlemskapAnnenForelderProps> = (
         <VilkårSkjema
             vilkårSkjemaContext={vilkårSkjemaContext}
             visVurderesEtter={false}
-            visSpørsmål={true}
+            visSpørsmål={false}
             vilkårResultat={vilkårResultat}
             vilkårFraConfig={vilkårFraConfig}
             toggleForm={toggleForm}
             person={person}
             lesevisning={lesevisning}
-        />
+        >
+            <FamilieRadioGruppe
+                legend={vilkårFraConfig.spørsmål ? vilkårFraConfig.spørsmål() : ''}
+                feil={
+                    vilkårSkjemaContext.skjema.visFeilmeldinger
+                        ? vilkårSkjemaContext.skjema.felter.resultat.feilmelding
+                        : ''
+                }
+                erLesevisning={lesevisning}
+            >
+                <Radio
+                    label={'Ja'}
+                    name={`${vilkårResultat.vilkårType}_${vilkårResultat.id}`}
+                    checked={vilkårSkjemaContext.skjema.felter.resultat.verdi === Resultat.OPPFYLT}
+                    onChange={() => {
+                        vilkårSkjemaContext.skjema.felter.resultat.validerOgSettFelt(
+                            Resultat.OPPFYLT
+                        );
+                    }}
+                />
+                <Radio
+                    label={'Nei'}
+                    name={`${vilkårResultat.vilkårType}_${vilkårResultat.id}`}
+                    checked={
+                        vilkårSkjemaContext.skjema.felter.resultat.verdi === Resultat.IKKE_OPPFYLT
+                    }
+                    onChange={() => {
+                        vilkårSkjemaContext.skjema.felter.resultat.validerOgSettFelt(
+                            Resultat.IKKE_OPPFYLT
+                        );
+                    }}
+                />
+                <Radio
+                    label={'Ikke aktuelt'}
+                    name={`${vilkårResultat.vilkårType}_${vilkårResultat.id}`}
+                    checked={
+                        vilkårSkjemaContext.skjema.felter.resultat.verdi === Resultat.IKKE_AKTUELT
+                    }
+                    onChange={() => {
+                        vilkårSkjemaContext.skjema.felter.resultat.validerOgSettFelt(
+                            Resultat.IKKE_AKTUELT
+                        );
+                    }}
+                />
+            </FamilieRadioGruppe>
+        </VilkårSkjema>
     );
 };
