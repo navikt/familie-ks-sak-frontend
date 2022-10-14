4186e24311fca305fcccc96a3a931729172ccd63
bragejahren <70642183+bragejahren@users.noreply.github.com>
2022-10-14 12:41:59 +0200

    Fikset fom-validering for alle vilkår samt antallTimer validering for Barnehageplass (#29)
    * Fikset fom-validering for alle vilkår samt antallTimer validering for Barnehageplass
    
    * Fikset feilende test

--- src/frontend/utils/test/validators.test.ts
+++ src/frontend/utils/test/validators.test.ts
@@ -103,7 +103,7 @@ describe('utils/validators', () => {
         });
         expect(valideringsresultat.valideringsstatus).toEqual(Valideringsstatus.FEIL);
         expect(valideringsresultat.feilmelding).toEqual(
-            'Du kan ikke legge til periode før barnet har fylt 1 år'
+            'Du kan ikke legge til periode før barnets fødselsdato'
         );
     });
 
