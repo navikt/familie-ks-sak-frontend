c61a7e0975e909c94d812293b23068bdb5760b72
Saikat Halder <saikat.halder@nav.no>
2022-11-04 11:53:56 +0100

    Fikset Frontend slik at Henlagt Behandling viser i lesevisning

--- src/frontend/context/behandlingContext/test/BehanldingContext.test.ts
+++ src/frontend/context/behandlingContext/test/BehanldingContext.test.ts
@@ -1,31 +1,66 @@
-import { BehandlingSteg } from '../../../typer/behandling';
+import { BehandlingStatus, BehandlingSteg } from '../../../typer/behandling';
 import { saksbehandlerHarKunLesevisning } from '../util';
 
 describe('BehandlingContext', () => {
     describe('erBehandlingILesevisning', () => {
         test('Skal returnere true dersom saksbehandler ikke har skrivetilgang.', () => {
             expect(
-                saksbehandlerHarKunLesevisning(false, true, BehandlingSteg.REGISTRERE_SØKNAD)
+                saksbehandlerHarKunLesevisning(
+                    false,
+                    true,
+                    BehandlingSteg.REGISTRERE_SØKNAD,
+                    BehandlingStatus.UTREDES
+                )
             ).toBeTruthy();
+        });
+        test('Skal returnere false dersom behandling er avsluttet.', () => {
+            expect(
+                saksbehandlerHarKunLesevisning(
+                    true,
+                    true,
+                    BehandlingSteg.REGISTRERE_SØKNAD,
+                    BehandlingStatus.AVSLUTTET
+                )
+            ).toBeFalsy();
         });
         test('Skal returnere false dersom saksbehandler har skrivetilgang og steget er før beslutte vedtak.', () => {
             expect(
-                saksbehandlerHarKunLesevisning(true, true, BehandlingSteg.SEND_TIL_BESLUTTER)
+                saksbehandlerHarKunLesevisning(
+                    true,
+                    true,
+                    BehandlingSteg.SEND_TIL_BESLUTTER,
+                    BehandlingStatus.UTREDES
+                )
             ).toBeFalsy();
         });
         test('Skal returnere true dersom saksbehandler har skrivetilgang, men ikke tilgang til enhet og steget er før beslutte vedtak.', () => {
             expect(
-                saksbehandlerHarKunLesevisning(true, false, BehandlingSteg.SEND_TIL_BESLUTTER)
+                saksbehandlerHarKunLesevisning(
+                    true,
+                    false,
+                    BehandlingSteg.SEND_TIL_BESLUTTER,
+                    BehandlingStatus.UTREDES
+                )
             ).toBeTruthy();
         });
         test('Skal returnere true dersom saksbehandler har skrivetilgang, men steget er likt eller etter beslutte vedtak.', () => {
             expect(
-                saksbehandlerHarKunLesevisning(true, true, BehandlingSteg.BESLUTTE_VEDTAK)
+                saksbehandlerHarKunLesevisning(
+                    true,
+                    true,
+                    BehandlingSteg.BESLUTTE_VEDTAK,
+                    BehandlingStatus.FATTER_VEDTAK
+                )
             ).toBeTruthy();
         });
         test('Skal returnere true dersom saksbehandler har skrivetilgang, men ikke tilgang til enhet.', () => {
             expect(
-                saksbehandlerHarKunLesevisning(true, false, BehandlingSteg.BESLUTTE_VEDTAK)
+                saksbehandlerHarKunLesevisning(
+                    true,
+                    false,
+                    BehandlingSteg.BESLUTTE_VEDTAK,
+                    BehandlingStatus.FATTER_VEDTAK
+                )
             ).toBeTruthy();
         });
         test('Skal returnere false dersom saksbehandler har skrivetilgang, men ikke tilgang til enhet. Men man skal ikke sjekke tilgang til enhet.', () => {
@@ -34,6 +69,7 @@ describe('BehandlingContext', () => {
                     true,
                     false,
                     BehandlingSteg.SEND_TIL_BESLUTTER,
+                    BehandlingStatus.FATTER_VEDTAK,
                     false
                 )
             ).toBeFalsy();
