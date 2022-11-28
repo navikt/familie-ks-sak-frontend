10adc4f880366e94e6894f6d9e63013bf4c4a8fa
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2022-11-28 08:49:24 +0100

    Endre tekst i utbetalingsoversikten

--- src/frontend/komponenter/Fagsak/Saksoversikt/PersonUtbetaling.tsx
+++ src/frontend/komponenter/Fagsak/Saksoversikt/PersonUtbetaling.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BodyShort } from '@navikt/ds-react';
 
 import type { IUtbetalingsperiodeDetalj } from '../../../typer/vedtaksperiode';
-import { formaterBeløp, hentAlder } from '../../../utils/formatter';
+import { formaterBeløp } from '../../../utils/formatter';
 import DashedHr from '../../Felleskomponenter/DashedHr/DashedHr';
 import PersonInformasjon from '../../Felleskomponenter/PersonInformasjon/PersonInformasjon';
 
@@ -12,9 +12,6 @@ interface IPersonUtbetalingProps {
 }
 
 const PersonUtbetaling: React.FC<IPersonUtbetalingProps> = ({ utbetalingsperiodeDetaljer }) => {
-    const genererTekstForOrdinær = (fødselsdato: string) =>
-        hentAlder(fødselsdato) < 6 ? 'Ordinær (under 6 år)' : 'Ordinær (fra 6 år)';
-
     return (
         <li>
             <PersonInformasjon person={utbetalingsperiodeDetaljer[0].person} />
@@ -22,11 +19,7 @@ const PersonUtbetaling: React.FC<IPersonUtbetalingProps> = ({ utbetalingsperiode
                 {utbetalingsperiodeDetaljer.map((utbetalingsperiodeDetalj, index) => {
                     return (
                         <div key={index} className={'saksoversikt__utbetalinger__ytelselinje'}>
-                            <BodyShort>
-                                {genererTekstForOrdinær(
-                                    utbetalingsperiodeDetalj.person.fødselsdato
-                                )}
-                            </BodyShort>
+                            <BodyShort>Kontantstøtte</BodyShort>
                             <BodyShort>
                                 {formaterBeløp(utbetalingsperiodeDetalj.utbetaltPerMnd)}
                             </BodyShort>
