de86b52a2e6b150d60f68738f461419a4415bfea
halvormundal <halvorbmundal@gmail.com>
2022-12-13 10:28:25 +0100

    opprett-klage -> opprett-klagebehandling

--- src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/OpprettBehandling/useOpprettBehandling.ts
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/OpprettBehandling/useOpprettBehandling.ts
@@ -169,7 +169,7 @@ const useOpprettBehandling = ({
         onSubmit<{ kravMottattDato: FamilieIsoDate }>(
             {
                 method: 'POST',
-                url: `/familie-ks-sak/api/fagsaker/${fagsakId}/opprett-klage`,
+                url: `/familie-ks-sak/api/fagsaker/${fagsakId}/opprett-klagebehandling`,
                 data: { kravMottattDato: kravMottattDato.verdi },
             },
             response => {
