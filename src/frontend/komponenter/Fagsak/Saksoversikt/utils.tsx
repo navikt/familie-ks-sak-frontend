dc864a20061dedaa413e395a5ecb2dc7d45308a7
halvormundal <halvorbmundal@gmail.com>
2022-12-21 10:02:59 +0100

    TILBAKBETALING -> TILBAKEBETALING

--- src/frontend/komponenter/Fagsak/Saksoversikt/utils.tsx
+++ src/frontend/komponenter/Fagsak/Saksoversikt/utils.tsx
@@ -25,7 +25,7 @@ import type { VisningBehandling } from './visningBehandling';
 
 export enum Saksoversiktstype {
     KONTANTSTØTTE = 'KONTANTSTØTTE',
-    TILBAKEBETALING = 'TILBAKBETALING',
+    TILBAKEBETALING = 'TILBAKEBETALING',
     KLAGE = 'KLAGE',
 }
 
