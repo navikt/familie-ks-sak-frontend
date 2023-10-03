b42afc9ef8feb4dff7d13b27685188d76e72a810
Daphne Leebeek <daphne.leebeek@nav.no>
2023-10-03 15:50:18 +0200

    Fjern nested div's og lag navigasjon mer lik oppgavebenken

--- src/frontend/komponenter/Barnehagebarn/BarnehagebarnListNavigator.tsx
+++ src/frontend/komponenter/Barnehagebarn/BarnehagebarnListNavigator.tsx
@@ -8,67 +8,74 @@ import { RessursStatus } from '@navikt/familie-typer';
 
 import { useBarnehagebarn } from '../../context/BarnehagebarnContext';
 
+const NavigasjonsContainer = styled.div`
+    margin-bottom: 1rem;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+`;
+
 const StyledSelect = styled(Select)`
-    max-width: 200px;
+    margin-right: auto;
+`;
+
+const FlexDiv = styled.div`
+    display: flex;
+    gap: 1rem;
+    align-items: center;
 `;
 const BarnehagebarnListNavigator: React.FunctionComponent = () => {
     const { barnehagebarnResponse, updateOffset, updateLimit, barnehagebarnRequestParams } =
         useBarnehagebarn();
     return barnehagebarnResponse.status === RessursStatus.SUKSESS &&
         barnehagebarnResponse.data.content.length >= 0 ? (
-        <div className="barnhagebarn-navigasjon-wrapper">
-            <div className="barnhagebarn-navigasjon">
-                <div className="barnhagebarn-navigasjon-left">
-                    <StyledSelect
-                        hideLabel
-                        label="Antall per side"
-                        size="small"
-                        aria-labelledby={'Antall per side'}
-                        value={barnehagebarnRequestParams.limit}
-                        onChange={event => updateLimit(+event.target.value)}
-                    >
-                        <option value="1">Vis 1 per side</option>
-                        <option value="2">Vis 2 per side</option>
-                        <option value="3">Vis 3 per side</option>{' '}
-                        <option value="4">Vis 4 per side</option>
-                        <option value="20">Vis 20 per side</option>
-                        <option value="50">Vis 50 per side</option>
-                        <option value="100">Vis 100 per side</option>
-                        <option value="200">Vis 200 per side</option>
-                    </StyledSelect>
-                </div>
-                <div className="barnhagebarn-navigasjon-center">
-                    {barnehagebarnResponse?.data?.totalPages > 0 && (
-                        <Pagination
-                            size="small"
-                            page={barnehagebarnResponse.data.number + 1}
-                            count={barnehagebarnResponse.data.totalPages}
-                            onPageChange={(side: number) => updateOffset(side - 1)}
-                            boundaryCount={1}
-                            siblingCount={1}
-                        />
-                    )}
-                </div>
-                <div className="barnhagebarn-navigasjon-right">
-                    {barnehagebarnResponse.data.totalElements > 0 ? (
-                        <div>
-                            <b>
-                                Side {barnehagebarnResponse.data.number + 1} av{' '}
-                                {barnehagebarnResponse.data.totalPages}
-                            </b>{' '}
-                            <span>
-                                ({barnehagebarnResponse.data.pageable.offset + 1} -{' '}
-                                {barnehagebarnResponse.data.pageable.offset +
-                                    barnehagebarnResponse.data.numberOfElements}{' '}
-                                av {barnehagebarnResponse.data.totalElements} totalt)
-                            </span>
-                        </div>
-                    ) : (
-                        <div>Ingen resultater</div>
-                    )}
-                </div>
-            </div>
-        </div>
+        <NavigasjonsContainer>
+            <StyledSelect
+                hideLabel
+                label="Antall per side"
+                size="small"
+                aria-labelledby={'Antall per side'}
+                value={barnehagebarnRequestParams.limit}
+                onChange={event => updateLimit(+event.target.value)}
+            >
+                <option value="1">Vis 1 per side</option>
+                <option value="2">Vis 2 per side</option>
+                <option value="3">Vis 3 per side</option>
+                <option value="4">Vis 4 per side</option>
+                <option value="20">Vis 20 per side</option>
+                <option value="50">Vis 50 per side</option>
+                <option value="100">Vis 100 per side</option>
+                <option value="200">Vis 200 per side</option>
+            </StyledSelect>
+            {barnehagebarnResponse.data.totalElements > 0 ? (
+                <FlexDiv>
+                    |
+                    <span>
+                        <b>
+                            Side {barnehagebarnResponse.data.number + 1} av{' '}
+                            {barnehagebarnResponse.data.totalPages}{' '}
+                        </b>
+                        ({barnehagebarnResponse.data.pageable.offset + 1} -{' '}
+                        {barnehagebarnResponse.data.pageable.offset +
+                            barnehagebarnResponse.data.numberOfElements}{' '}
+                        av {barnehagebarnResponse.data.totalElements} totalt)
+                    </span>
+                    |
+                </FlexDiv>
+            ) : (
+                <div>Ingen resultater</div>
+            )}
+            {barnehagebarnResponse?.data?.totalPages > 0 && (
+                <Pagination
+                    size="small"
+                    page={barnehagebarnResponse.data.number + 1}
+                    count={barnehagebarnResponse.data.totalPages}
+                    onPageChange={(side: number) => updateOffset(side - 1)}
+                    boundaryCount={1}
+                    siblingCount={1}
+                />
+            )}
+        </NavigasjonsContainer>
     ) : null;
 };
 export default BarnehagebarnListNavigator;
