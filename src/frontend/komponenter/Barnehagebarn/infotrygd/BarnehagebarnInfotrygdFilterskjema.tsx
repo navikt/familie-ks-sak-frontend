4b92241133b65e9c5f791a2bf4fe1b0e6fc83bb6
Daphne Leebeek <daphne.leebeek@nav.no>
2023-10-03 15:46:14 +0200

    Rydd opp nesting av divs og ikonstyling

--- src/frontend/komponenter/Barnehagebarn/infotrygd/BarnehagebarnInfotrygdFilterskjema.tsx
+++ src/frontend/komponenter/Barnehagebarn/infotrygd/BarnehagebarnInfotrygdFilterskjema.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 import { FunnelIcon, FileResetIcon } from '@navikt/aksel-icons';
-import { Button, Checkbox, Fieldset, Label, TextField } from '@navikt/ds-react';
+import { Button, Checkbox, Fieldset, TextField } from '@navikt/ds-react';
 
 import { useBarnehagebarnInfotrygd } from '../../../context/BarnehagebarnInfotrygdContext';
+
+const FlexDiv = styled.div`
+    display: flex;
+    align-items: end;
+    gap: 1rem;
+    margin-bottom: 1rem;
+`;
+
+const StyledFieldset = styled(Fieldset)`
+    margin-top: 1rem;
+`;
+
 const BarnehagebarnInfotrygdFilterskjema: React.FunctionComponent = () => {
     const {
         barnehagebarnRequestParams,
@@ -15,79 +29,64 @@ const BarnehagebarnInfotrygdFilterskjema: React.FunctionComponent = () => {
     } = useBarnehagebarnInfotrygd();
 
     return (
-        <Fieldset
+        <StyledFieldset
             size="small"
-            className="barnehagebarnskjema"
             aria-label="barnehagebarn-filterskjema"
             legend="Filterskjema"
             description="Filtrer barnehagebarn resultater"
             hideLegend
         >
-            <div className="barnhagebarn-filtere">
-                <div className="bb-filter">
-                    <TextField
-                        label="Barns ident"
-                        title="Filtrer på barns ident"
-                        size="small"
-                        maxLength={11}
-                        value={barnehagebarnRequestParams.ident}
-                        onChange={(event): void => {
-                            updateIdent(event.target.value);
-                        }}
-                    />
-                </div>
-                <div className="bb-filter">
-                    <TextField
-                        label="Kommunenavn"
-                        title="Filtrer på kommunenavn"
-                        size="small"
-                        maxLength={200}
-                        value={barnehagebarnRequestParams.kommuneNavn}
-                        onChange={(event): void => {
-                            updateKommuneNavn(event.target.value);
-                        }}
-                    />
-                </div>
-                <div className="bb-filter">
-                    <Label size="small" htmlFor="kun-loepende-fagsak">
-                        Kun løpende fagsaker
-                    </Label>
-                    <Checkbox
-                        id="kun-loepende-fagsak"
-                        title="Vis kun løpende fagsaker"
-                        checked={barnehagebarnRequestParams.kunLøpendeFagsak}
-                        hideLabel
-                        onChange={(): void => {
-                            updateKunLøpendeFagsak();
-                        }}
-                    >
-                        Vis kun løpende fagsaker
-                    </Checkbox>
-                </div>
-            </div>
-            <div className="barnhagebarn-actions">
-                <div className="bb-filter">
-                    <Button
-                        variant="primary"
-                        size="small"
-                        onClick={() => hentBarnehagebarnResponseRessurs()}
-                    >
-                        <FunnelIcon title="Filtrer på angitte verdier" /> Filtrer
-                    </Button>
-                </div>
-                <div className="bb-filter">
-                    <Button
-                        variant="primary"
-                        size="small"
-                        onClick={() => {
-                            fjernBarnehagebarnFiltere();
-                        }}
-                    >
-                        <FileResetIcon title="Fjern filtrering" /> Fjern filtere
-                    </Button>
-                </div>
-            </div>
-        </Fieldset>
+            <FlexDiv>
+                <TextField
+                    label="Barns ident"
+                    title="Filtrer på barns ident"
+                    size="small"
+                    maxLength={11}
+                    value={barnehagebarnRequestParams.ident}
+                    onChange={(event): void => {
+                        updateIdent(event.target.value);
+                    }}
+                />
+                <TextField
+                    label="Kommunenavn"
+                    title="Filtrer på kommunenavn"
+                    size="small"
+                    maxLength={200}
+                    value={barnehagebarnRequestParams.kommuneNavn}
+                    onChange={(event): void => {
+                        updateKommuneNavn(event.target.value);
+                    }}
+                />
+                <Checkbox
+                    id="kun-loepende-fagsak"
+                    title="Vis kun løpende fagsaker"
+                    checked={barnehagebarnRequestParams.kunLøpendeFagsak}
+                    onChange={(): void => {
+                        updateKunLøpendeFagsak();
+                    }}
+                >
+                    Kun løpende fagsaker
+                </Checkbox>
+            </FlexDiv>
+            <FlexDiv>
+                <Button
+                    variant="primary"
+                    size="small"
+                    onClick={() => hentBarnehagebarnResponseRessurs()}
+                    icon={<FunnelIcon aria-hidden />}
+                ></Button>
+                <Button
+                    variant="primary"
+                    size="small"
+                    onClick={() => {
+                        fjernBarnehagebarnFiltere();
+                    }}
+                    icon={<FileResetIcon aria-hidden />}
+                >
+                    Fjern filtre
+                </Button>
+            </FlexDiv>
+        </StyledFieldset>
     );
 };
 
