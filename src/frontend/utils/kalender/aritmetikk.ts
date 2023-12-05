9c0e35606fab2c54af17fc876ff3520a994cd49c
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-12-05 12:42:08 +0100

    fjern kalenderDiffMåned som kun ble brukt i test

--- src/frontend/utils/kalender/aritmetikk.ts
+++ src/frontend/utils/kalender/aritmetikk.ts
@@ -1,8 +1,3 @@
-import type { DagMånedÅr } from '.';
-
 export const kalenderDiff = (første: Date, andre: Date) => {
     return første.getTime() - andre.getTime();
 };
-
-export const kalenderDiffMåned = (første: DagMånedÅr, andre: DagMånedÅr) =>
-    12 * (andre.år - første.år) + (andre.måned - første.måned);
