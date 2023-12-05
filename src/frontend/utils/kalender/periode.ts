70ec8d3be80aff8886328c9b6ba039473f9ddc22
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-12-05 12:30:57 +0100

    flytt og oppdater periodeDiff-funksjonalitet

--- src/frontend/utils/kalender/periode.ts
+++ src/frontend/utils/kalender/periode.ts
@@ -1,5 +1,4 @@
 import type { FamilieIsoDate, IPeriode, IYearMonthPeriode, YearMonth } from '.';
-import { kalenderDatoMedFallback, TIDENES_ENDE, kalenderDiff, kalenderDatoTilDate } from '.';
 
 export const nyPeriode = (fom?: FamilieIsoDate, tom?: FamilieIsoDate): IPeriode => {
     return {
@@ -13,14 +12,4 @@ export const nyYearMonthPeriode = (fom?: YearMonth, tom?: YearMonth): IYearMonth
         fom: fom !== '' && fom ? fom : undefined,
         tom: tom !== '' && tom ? tom : undefined,
     };
-};
-
-export const periodeDiff = (første: IPeriode, andre: IPeriode) => {
-    if (!første.fom && !første.tom) {
-        return 1;
-    }
-    return kalenderDiff(
-        kalenderDatoTilDate(kalenderDatoMedFallback(første.fom, TIDENES_ENDE)),
-        kalenderDatoTilDate(kalenderDatoMedFallback(andre.fom, TIDENES_ENDE))
-    );
 };
