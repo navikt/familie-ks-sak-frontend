eff45549f99297e8260dfeed1bfab3742a68a42d
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-12-06 15:38:39 +0100

    Fjern ubrukte funksjoner

--- src/frontend/utils/kalender/kalender.ts
+++ src/frontend/utils/kalender/kalender.ts
@@ -1,28 +1,4 @@
 import { parseIso8601String } from './io';
 import type { DagMånedÅr, FamilieIsoDate } from './typer';
 
-export const TIDENES_MORGEN: DagMånedÅr = {
-    dag: 1,
-    måned: 1,
-    år: 1000,
-};
-
 export const kalenderDato = (dato: FamilieIsoDate): DagMånedÅr => parseIso8601String(dato);
-
-export const kalenderDatoMedFallback = (
-    dato: FamilieIsoDate | undefined,
-    fallbackDato: DagMånedÅr
-): DagMånedÅr => (dato ? parseIso8601String(dato) : fallbackDato);
-
-export const kalenderDatoTilDate = (
-    dagMånedÅr: DagMånedÅr,
-    timer?: number,
-    minutter?: number
-): Date =>
-    new Date(
-        dagMånedÅr.år,
-        dagMånedÅr.måned,
-        dagMånedÅr.dag,
-        timer ? timer : 0,
-        minutter ? minutter : 0
-    );
