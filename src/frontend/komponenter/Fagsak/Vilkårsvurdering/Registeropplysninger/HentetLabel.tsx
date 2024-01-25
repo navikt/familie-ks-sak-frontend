eca6433eec064e29e84da6a5464882f7ff9fa580
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-01-25 16:17:37 +0100

    Bruk styling på Detail direkte

--- src/frontend/komponenter/Fagsak/Vilkårsvurdering/Registeropplysninger/HentetLabel.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-import { Detail } from '@navikt/ds-react';
-import { AGray600 } from '@navikt/ds-tokens/dist/tokens';
-
-export const HentetLabel = styled(Detail)`
-    color: ${AGray600};
-`;
