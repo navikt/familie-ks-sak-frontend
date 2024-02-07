85adb963ae0c17cc5cd13891811190aefdd90c48
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-02-07 09:26:51 +0100

    Begrunnelser: EØS-avslag (#549)
    * Vis EØS avslagstekster hvis vilkår vurderes etter EØS
    
    * Vis eøs-avslag hvis vurderes etter eøs, og sørg for gruppering av valg i menyen
    
    * Legg på prefix på begrunnelser etter endringer backend
    
    * Update src/frontend/komponenter/Fagsak/Vilkårsvurdering/GeneriskVilkår/AvslagBegrunnelseMultiselect.tsx
    
    * Update src/frontend/komponenter/Fagsak/Vilkårsvurdering/GeneriskVilkår/AvslagBegrunnelseMultiselect.tsx
    
    * Map om til begrunnelsetype så vi slipper å caste

--- src/frontend/komponenter/Fagsak/Vilkårsvurdering/GeneriskVilkår/useAvslagBegrunnelseMultiselect.ts
+++ src/frontend/komponenter/Fagsak/Vilkårsvurdering/GeneriskVilkår/useAvslagBegrunnelseMultiselect.ts
@@ -1,22 +1,52 @@
+import type { GroupBase } from 'react-select';
+
+import type { ISelectOption } from '@navikt/familie-form-elements';
 import { RessursStatus } from '@navikt/familie-typer';
 
 import type { IRestBegrunnelseTilknyttetVilkår } from '../../../../typer/vedtak';
+import { BegrunnelseType, begrunnelseTyper } from '../../../../typer/vedtak';
 import type { VilkårType } from '../../../../typer/vilkår';
+import { Regelverk } from '../../../../typer/vilkår';
 import { useVedtaksbegrunnelseTekster } from '../../Vedtak/VedtakBegrunnelserTabell/Context/VedtaksbegrunnelseTeksterContext';
 
-const useAvslagBegrunnelseMultiselect = (vilkårType: VilkårType) => {
+const useAvslagBegrunnelseMultiselect = (vilkårType: VilkårType, regelverk?: Regelverk) => {
     const { vedtaksbegrunnelseTekster } = useVedtaksbegrunnelseTekster();
 
-    const avslagBegrunnelseTeksterForGjeldendeVilkår =
-        vedtaksbegrunnelseTekster.status === RessursStatus.SUKSESS
-            ? vedtaksbegrunnelseTekster.data.AVSLAG.filter(
-                  (begrunnelse: IRestBegrunnelseTilknyttetVilkår) =>
-                      begrunnelse.vilkår === vilkårType
-              )
-            : [];
+    if (vedtaksbegrunnelseTekster.status !== RessursStatus.SUKSESS) {
+        return { grupperteAvslagsbegrunnelser: [] };
+    }
+
+    const gyldigeBegrunnelseTyper = [BegrunnelseType.AVSLAG];
+
+    if (regelverk == Regelverk.EØS_FORORDNINGEN) {
+        gyldigeBegrunnelseTyper.push(BegrunnelseType.EØS_AVSLAG);
+    }
+
+    const grupperteBegrunnelser = Object.keys(vedtaksbegrunnelseTekster.data)
+        .map((type: string) => type as BegrunnelseType)
+        .filter((begrunnelseType: BegrunnelseType) =>
+            gyldigeBegrunnelseTyper.includes(begrunnelseType)
+        )
+        .reduce((acc: GroupBase<ISelectOption>[], begrunnelseType: BegrunnelseType) => {
+            return [
+                ...acc,
+                {
+                    label: begrunnelseTyper[begrunnelseType],
+                    options: vedtaksbegrunnelseTekster.data[begrunnelseType]
+                        .filter(
+                            (begrunnelse: IRestBegrunnelseTilknyttetVilkår) =>
+                                begrunnelse.vilkår === vilkårType
+                        )
+                        .map((begrunnelse: IRestBegrunnelseTilknyttetVilkår) => ({
+                            label: begrunnelse.navn,
+                            value: begrunnelse.id,
+                        })),
+                },
+            ];
+        }, []);
 
     return {
-        avslagBegrunnelseTeksterForGjeldendeVilkår,
+        grupperteAvslagsbegrunnelser: grupperteBegrunnelser,
     };
 };
 
