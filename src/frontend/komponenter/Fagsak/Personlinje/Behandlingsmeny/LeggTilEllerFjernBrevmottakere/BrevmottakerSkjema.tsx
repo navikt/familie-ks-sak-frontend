9763795da8cca3c0c661699fda75c92ade0c1bf3
Daphne Leebeek <daphne.leebeek@nav.no>
2024-02-08 11:50:02 +0100

    Refaktorer styling BrevmottakerSkjema

--- src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/LeggTilEllerFjernBrevmottakere/BrevmottakerSkjema.tsx
+++ src/frontend/komponenter/Fagsak/Personlinje/Behandlingsmeny/LeggTilEllerFjernBrevmottakere/BrevmottakerSkjema.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import { Fieldset, Select, TextField } from '@navikt/ds-react';
-import { ASpacing6 } from '@navikt/ds-tokens/dist/tokens';
+import { Fieldset, HStack, Select, Spacer, TextField, VStack } from '@navikt/ds-react';
 import type { ISkjema } from '@navikt/familie-skjema';
 import { Valideringsstatus } from '@navikt/familie-skjema';
 
@@ -13,27 +12,9 @@ import type { IBehandling } from '../../../../../typer/behandling';
 import { hentFrontendFeilmelding } from '../../../../../utils/ressursUtils';
 import { FamilieLandvelger } from '../../../Behandlingsresultat/EøsPeriode/FamilieLandvelger';
 
-const PostnummerOgStedContainer = styled.div`
-    display: grid;
-    grid-gap: 1rem;
-    grid-template-columns: 8rem 24rem;
-
-    &:has(.navds-text-field--error) {
-        .navds-form-field .navds-form-field__error {
-            height: 3rem;
-            display: initial;
-        }
-    }
-`;
-
-const StyledFieldset = styled(Fieldset)`
-    &.navds-fieldset > div:not(:first-of-type):not(:empty) {
-        margin-top: ${ASpacing6};
-    }
-`;
-
-const MottakerSelect = styled(Select)`
-    max-width: 19rem;
+const StyledTextField = styled(TextField)<{ $width: string }>`
+    width: ${props => props.$width};
+    height: fit-content;
 `;
 
 interface Props {
@@ -47,94 +28,109 @@ const BrevmottakerSkjema = ({ erLesevisning, skjema, navnErPreutfylt }: Props) =
 
     return (
         <>
-            <StyledFieldset
+            <Fieldset
                 legend="Skjema for å legge til eller fjerne brevmottaker"
                 hideLegend
                 error={skjema.visFeilmeldinger && hentFrontendFeilmelding(skjema.submitRessurs)}
             >
-                <MottakerSelect
-                    {...skjema.felter.mottaker.hentNavBaseSkjemaProps(skjema.visFeilmeldinger)}
-                    readOnly={erLesevisning}
-                    label="Mottaker"
-                    onChange={(event: React.ChangeEvent<HTMLSelectElement>): void => {
-                        skjema.felter.mottaker.validerOgSettFelt(event.target.value as Mottaker);
-                    }}
-                >
-                    <option value="">Velg</option>
-                    {gyldigeMottakerTyper.map(mottaker => (
-                        <option value={mottaker} key={mottaker}>
-                            {mottakerVisningsnavn[mottaker]}
-                        </option>
-                    ))}
-                </MottakerSelect>
-                <TextField
-                    {...skjema.felter.navn.hentNavBaseSkjemaProps(skjema.visFeilmeldinger)}
-                    readOnly={erLesevisning || navnErPreutfylt}
-                    label={'Navn'}
-                    onChange={(event): void => {
-                        skjema.felter.navn.validerOgSettFelt(event.target.value);
-                    }}
-                />
-                <TextField
-                    {...skjema.felter.adresselinje1.hentNavBaseSkjemaProps(skjema.visFeilmeldinger)}
-                    readOnly={erLesevisning}
-                    label={'Adresselinje 1'}
-                    onChange={(event): void => {
-                        skjema.felter.adresselinje1.validerOgSettFelt(event.target.value);
-                    }}
-                />
-                <TextField
-                    {...skjema.felter.adresselinje2.hentNavBaseSkjemaProps(skjema.visFeilmeldinger)}
-                    readOnly={erLesevisning}
-                    label={'Adresselinje 2 (valgfri)'}
-                    onChange={(event): void => {
-                        skjema.felter.adresselinje2.validerOgSettFelt(event.target.value);
-                    }}
-                />
-                <PostnummerOgStedContainer>
+                <VStack gap={'6'}>
+                    <Select
+                        {...skjema.felter.mottaker.hentNavBaseSkjemaProps(skjema.visFeilmeldinger)}
+                        readOnly={erLesevisning}
+                        label="Mottaker"
+                        onChange={(event: React.ChangeEvent<HTMLSelectElement>): void => {
+                            skjema.felter.mottaker.validerOgSettFelt(
+                                event.target.value as Mottaker
+                            );
+                        }}
+                    >
+                        <option value="">Velg</option>
+                        {gyldigeMottakerTyper.map(mottaker => (
+                            <option value={mottaker} key={mottaker}>
+                                {mottakerVisningsnavn[mottaker]}
+                            </option>
+                        ))}
+                    </Select>
                     <TextField
-                        {...skjema.felter.postnummer.hentNavBaseSkjemaProps(
+                        {...skjema.felter.navn.hentNavBaseSkjemaProps(skjema.visFeilmeldinger)}
+                        readOnly={erLesevisning || navnErPreutfylt}
+                        label={'Navn'}
+                        onChange={(event): void => {
+                            skjema.felter.navn.validerOgSettFelt(event.target.value);
+                        }}
+                    />
+                    <TextField
+                        {...skjema.felter.adresselinje1.hentNavBaseSkjemaProps(
                             skjema.visFeilmeldinger
                         )}
                         readOnly={erLesevisning}
-                        label={'Postnummer'}
+                        label={'Adresselinje 1'}
                         onChange={(event): void => {
-                            skjema.felter.postnummer.validerOgSettFelt(event.target.value);
+                            skjema.felter.adresselinje1.validerOgSettFelt(event.target.value);
                         }}
                     />
                     <TextField
-                        {...skjema.felter.poststed.hentNavBaseSkjemaProps(skjema.visFeilmeldinger)}
+                        {...skjema.felter.adresselinje2.hentNavBaseSkjemaProps(
+                            skjema.visFeilmeldinger
+                        )}
                         readOnly={erLesevisning}
-                        label={'Poststed'}
+                        label={'Adresselinje 2 (valgfri)'}
                         onChange={(event): void => {
-                            skjema.felter.poststed.validerOgSettFelt(event.target.value);
+                            skjema.felter.adresselinje2.validerOgSettFelt(event.target.value);
                         }}
                     />
-                </PostnummerOgStedContainer>
+                    <HStack>
+                        <StyledTextField
+                            {...skjema.felter.postnummer.hentNavBaseSkjemaProps(
+                                skjema.visFeilmeldinger
+                            )}
+                            readOnly={erLesevisning}
+                            label={'Postnummer'}
+                            onChange={(event): void => {
+                                skjema.felter.postnummer.validerOgSettFelt(event.target.value);
+                            }}
+                            $width={'10rem'}
+                        />
+                        <Spacer />
+                        <StyledTextField
+                            {...skjema.felter.poststed.hentNavBaseSkjemaProps(
+                                skjema.visFeilmeldinger
+                            )}
+                            readOnly={erLesevisning}
+                            label={'Poststed'}
+                            onChange={(event): void => {
+                                skjema.felter.poststed.validerOgSettFelt(event.target.value);
+                            }}
+                            $width={'19.5rem'}
+                        />
+                    </HStack>
 
-                <FamilieLandvelger
-                    id={'land'}
-                    value={skjema.felter.land.verdi !== '' ? skjema.felter.land.verdi : undefined}
-                    label={'Land'}
-                    medFlag
-                    utenMargin
-                    eksluderLand={
-                        skjema.felter.mottaker.verdi === Mottaker.BRUKER_MED_UTENLANDSK_ADRESSE
-                            ? ['NO']
-                            : undefined
-                    }
-                    feil={
-                        skjema.visFeilmeldinger &&
-                        skjema.felter.land.valideringsstatus === Valideringsstatus.FEIL
-                            ? skjema.felter.land.feilmelding?.toString()
-                            : ''
-                    }
-                    erLesevisning={erLesevisning}
-                    onChange={land => {
-                        skjema.felter.land.validerOgSettFelt(land.value);
-                    }}
-                />
-            </StyledFieldset>
+                    <FamilieLandvelger
+                        id={'land'}
+                        value={
+                            skjema.felter.land.verdi !== '' ? skjema.felter.land.verdi : undefined
+                        }
+                        label={'Land'}
+                        medFlag
+                        utenMargin
+                        eksluderLand={
+                            skjema.felter.mottaker.verdi === Mottaker.BRUKER_MED_UTENLANDSK_ADRESSE
+                                ? ['NO']
+                                : undefined
+                        }
+                        feil={
+                            skjema.visFeilmeldinger &&
+                            skjema.felter.land.valideringsstatus === Valideringsstatus.FEIL
+                                ? skjema.felter.land.feilmelding?.toString()
+                                : ''
+                        }
+                        erLesevisning={erLesevisning}
+                        onChange={land => {
+                            skjema.felter.land.validerOgSettFelt(land.value);
+                        }}
+                    />
+                </VStack>
+            </Fieldset>
         </>
     );
 };
