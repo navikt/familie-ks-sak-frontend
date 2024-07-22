16c51654ec0a737b0341ed1573b688ca7a081cad
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-07-22 10:21:55 +0200

    Bug: styling på oppgavebenken (#768)
    * legg div rundt select så errormelding ikke skaper rar styling
    
    * trekk switch-case ut i egen funksjon for å gjøre koden lettere å lese
    
    * fiks knappestyling
    
    * fiks mellomrom mellom filtrering og knapper
    
    * fiks mellomrom mellom ulike seksjoner på oppgavebenken

--- src/frontend/komponenter/Oppgavebenk/FilterSkjema.tsx
+++ src/frontend/komponenter/Oppgavebenk/FilterSkjema.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { type ReactElement } from 'react';
 
 import styled from 'styled-components';
 
-import { Button, Fieldset, Select } from '@navikt/ds-react';
+import { Button, Fieldset, HStack, Select, VStack } from '@navikt/ds-react';
 import { Valideringsstatus } from '@navikt/familie-skjema';
 import { RessursStatus } from '@navikt/familie-typer';
 
@@ -17,34 +17,6 @@ const DatoVelgerContainer = styled.div`
     max-width: 12.5rem;
 `;
 
-// Denne stylingen skal fjernes på sikt (minus marginer)
-const StyledButton = styled(Button)`
-    margin-top: 0.5rem;
-    margin-right: 1.5rem;
-    padding: calc(0.25rem - 1px) 1.5rem calc(0.25rem - 1px) 1.5rem;
-    font-weight: bolder;
-    min-height: 2rem;
-    .navds-button__inner {
-        font-weight: 600;
-        letter-spacing: 0.0625em;
-    }
-`;
-
-const StyledFieldset = styled(Fieldset)`
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 2rem;
-`;
-
-const FilterRad = styled.div`
-    display: flex;
-    margin-top: 1rem;
-
-    & > * {
-        padding-right: 1.5rem;
-    }
-`;
-
 const FilterSkjema: React.FunctionComponent = () => {
     const { innloggetSaksbehandler } = useApp();
     const {
@@ -56,100 +28,98 @@ const FilterSkjema: React.FunctionComponent = () => {
         validerSkjema,
     } = useOppgaver();
 
-    return (
-        <StyledFieldset legend={'Oppgavebenken filterskjema'} hideLegend>
-            <FilterRad>
-                {Object.values(oppgaveFelter)
-                    .filter((oppgaveFelt: IOppgaveFelt) => oppgaveFelt.filter)
-                    .map((oppgaveFelt: IOppgaveFelt) => {
-                        switch (oppgaveFelt.filter?.type) {
-                            case 'dato':
-                                return (
-                                    <DatoVelgerContainer key={oppgaveFelt.nøkkel}>
-                                        <DatovelgerForGammelSkjemaløsning
-                                            key={oppgaveFelt.nøkkel}
-                                            label={oppgaveFelt.label}
-                                            onDateChange={(dato?: IsoDatoString) => {
-                                                settVerdiPåOppgaveFelt(
-                                                    oppgaveFelt,
-                                                    dato ? dato : ''
-                                                );
-                                            }}
-                                            value={oppgaveFelt.filter.selectedValue}
-                                            visFeilmeldinger={
-                                                oppgaveFelt.valideringsstatus ===
-                                                Valideringsstatus.FEIL
-                                            }
-                                            feilmelding={oppgaveFelt.feilmelding}
-                                        />
-                                    </DatoVelgerContainer>
-                                );
-                            case 'select':
-                                return (
-                                    <Select
-                                        label={oppgaveFelt.label}
-                                        onChange={event =>
-                                            settVerdiPåOppgaveFelt(oppgaveFelt, event.target.value)
-                                        }
-                                        key={oppgaveFelt.nøkkel}
-                                        value={oppgaveFelt.filter.selectedValue}
-                                        error={
-                                            oppgaveFelt.valideringsstatus === Valideringsstatus.FEIL
-                                                ? oppgaveFelt.feilmelding
-                                                : undefined
-                                        }
-                                        data-cy={`select-${oppgaveFelt.label}`}
-                                    >
-                                        {oppgaveFelt.filter.nøkkelPar &&
-                                            Object.values(oppgaveFelt.filter.nøkkelPar)
-                                                .filter((par: IPar) =>
-                                                    oppgaveFelt.erSynlig
-                                                        ? oppgaveFelt.erSynlig(
-                                                              par,
-                                                              innloggetSaksbehandler
-                                                          )
-                                                        : true
-                                                )
-                                                .map((par: IPar) => {
-                                                    return (
-                                                        <option
-                                                            aria-selected={
-                                                                oppgaveFelt.filter &&
-                                                                oppgaveFelt.filter.selectedValue ===
-                                                                    par.id
-                                                            }
-                                                            key={par.id}
-                                                            value={par.id}
-                                                        >
-                                                            {par.navn}
-                                                        </option>
-                                                    );
-                                                })}
-                                    </Select>
-                                );
-                            default:
-                                return null;
-                        }
-                    })}
-            </FilterRad>
+    function tilOppgaveFeltKomponent(oppgaveFelt: IOppgaveFelt): ReactElement | null {
+        switch (oppgaveFelt.filter?.type) {
+            case 'dato':
+                return (
+                    <DatoVelgerContainer key={oppgaveFelt.nøkkel}>
+                        <DatovelgerForGammelSkjemaløsning
+                            key={oppgaveFelt.nøkkel}
+                            label={oppgaveFelt.label}
+                            onDateChange={(dato?: IsoDatoString) => {
+                                settVerdiPåOppgaveFelt(oppgaveFelt, dato ? dato : '');
+                            }}
+                            value={oppgaveFelt.filter.selectedValue}
+                            visFeilmeldinger={
+                                oppgaveFelt.valideringsstatus === Valideringsstatus.FEIL
+                            }
+                            feilmelding={oppgaveFelt.feilmelding}
+                        />
+                    </DatoVelgerContainer>
+                );
+            case 'select':
+                return (
+                    <div>
+                        <Select
+                            label={oppgaveFelt.label}
+                            onChange={event =>
+                                settVerdiPåOppgaveFelt(oppgaveFelt, event.target.value)
+                            }
+                            key={oppgaveFelt.nøkkel}
+                            value={oppgaveFelt.filter.selectedValue}
+                            error={
+                                oppgaveFelt.valideringsstatus === Valideringsstatus.FEIL
+                                    ? oppgaveFelt.feilmelding
+                                    : undefined
+                            }
+                            data-cy={`select-${oppgaveFelt.label}`}
+                        >
+                            {oppgaveFelt.filter.nøkkelPar &&
+                                Object.values(oppgaveFelt.filter.nøkkelPar)
+                                    .filter((par: IPar) =>
+                                        oppgaveFelt.erSynlig
+                                            ? oppgaveFelt.erSynlig(par, innloggetSaksbehandler)
+                                            : true
+                                    )
+                                    .map((par: IPar) => {
+                                        return (
+                                            <option
+                                                aria-selected={
+                                                    oppgaveFelt.filter &&
+                                                    oppgaveFelt.filter.selectedValue === par.id
+                                                }
+                                                key={par.id}
+                                                value={par.id}
+                                            >
+                                                {par.navn}
+                                            </option>
+                                        );
+                                    })}
+                        </Select>
+                    </div>
+                );
+            default:
+                return null;
+        }
+    }
 
-            <div>
-                <StyledButton
-                    variant="primary"
-                    onClick={() => {
-                        validerSkjema() && hentOppgaver();
-                    }}
-                    loading={oppgaver.status === RessursStatus.HENTER}
-                    disabled={oppgaver.status === RessursStatus.HENTER}
-                    children={'Hent oppgaver'}
-                />
-                <StyledButton
-                    onClick={tilbakestillOppgaveFelter}
-                    variant="secondary"
-                    children={'Tilbakestill filtrering'}
-                />
-            </div>
-        </StyledFieldset>
+    return (
+        <Fieldset legend={'Oppgavebenken filterskjema'} hideLegend>
+            <VStack gap="4">
+                <HStack gap="6">
+                    {Object.values(oppgaveFelter)
+                        .filter((oppgaveFelt: IOppgaveFelt) => oppgaveFelt.filter)
+                        .map(tilOppgaveFeltKomponent)}
+                </HStack>
+
+                <HStack gap="2">
+                    <Button
+                        variant="primary"
+                        onClick={() => {
+                            validerSkjema() && hentOppgaver();
+                        }}
+                        loading={oppgaver.status === RessursStatus.HENTER}
+                        disabled={oppgaver.status === RessursStatus.HENTER}
+                        children={'Hent oppgaver'}
+                    />
+                    <Button
+                        onClick={tilbakestillOppgaveFelter}
+                        variant="secondary"
+                        children={'Tilbakestill filtrering'}
+                    />
+                </HStack>
+            </VStack>
+        </Fieldset>
     );
 };
 
