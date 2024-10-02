35e98364c2f4723ad52ba70fbf561775a630c0ee
Hanne Olsen <hanne.olsen@nav.no>
2024-10-02 15:03:58 +0200

    Syncer Informasjonsbolk slik at den er lik versjonen i ba-sak

--- src/frontend/komponenter/Felleskomponenter/Informasjonsbolk/Informasjonsbolk.tsx
+++ src/frontend/komponenter/Felleskomponenter/Informasjonsbolk/Informasjonsbolk.tsx
@@ -1,44 +1,27 @@
 import * as React from 'react';
 
-import { styled } from 'styled-components';
-
-import { BodyShort, Label } from '@navikt/ds-react';
+import { BodyShort, HGrid } from '@navikt/ds-react';
 import { ATextDefault } from '@navikt/ds-tokens/dist/tokens';
 
-interface IInformasjon {
+interface IProps {
     label: string;
     tekst: string;
-    tekstTitle?: string;
+    tekstHover?: string;
+    tekstFarge?: string;
 }
 
-interface IProps {
-    informasjon: IInformasjon[];
-    infoTeksFarve?: string;
-}
-
-const InformasjonsbolkContainer = styled.div`
-    column-count: 2;
-    max-width: 30rem;
-    padding: 0.5rem 0;
-`;
-
-const Informasjonsbolk: React.FC<IProps> = ({ informasjon, infoTeksFarve }) => {
+const Informasjonsbolk: React.FC<IProps> = ({ label, tekst, tekstHover, tekstFarge }) => {
     return (
-        <InformasjonsbolkContainer>
-            {informasjon.map((info: IInformasjon) => {
-                return <BodyShort key={info.label + info.tekst} children={info.label} />;
-            })}
-            {informasjon.map((info: IInformasjon) => {
-                return (
-                    <Label
-                        style={{ color: infoTeksFarve ?? ATextDefault, display: 'block' }}
-                        title={info.tekstTitle}
-                        key={info.tekst + info.label}
-                        children={info.tekst}
-                    />
-                );
-            })}
-        </InformasjonsbolkContainer>
+        <HGrid columns={2}>
+            <BodyShort>{label}</BodyShort>
+            <BodyShort
+                weight="semibold"
+                style={{ color: tekstFarge ?? ATextDefault }}
+                title={tekstHover}
+            >
+                {tekst}
+            </BodyShort>
+        </HGrid>
     );
 };
 
