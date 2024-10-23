75f8df9c850f3f84d4ee69d6660e9a74c44a28ff
Hanne Olsen <hanne.olsen@nav.no>
2024-10-23 16:20:41 +0200

    Ta hensyn til kode 6, 7 og 19 i dokumentoversikten (#851)
    * Legger til kode for dokumentvisning som tar hensyn til kode 6, 7 og 19
    
    * Fikser riktig dokumenttittel når ingen tittel er satt
    
    * journalpostMedTilgang -> tilgangsstyrtJournalpost
    
    * Småfiks på visning og typer
    
    * Trekker tekstlogikk ut i egen konstant
    
    * Omskriver logikk for å gjøre den ryddigere
    
    * Fjerner data om hvilken adressebeskyttelsegradering som gjelder
    
    ---------
    
    Co-authored-by: bragejahren <70642183+bragejahren@users.noreply.github.com>

--- src/frontend/komponenter/Fagsak/journalposter/JournalpostDokument.tsx
+++ src/frontend/komponenter/Fagsak/journalposter/JournalpostDokument.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import { ExternalLink } from '@navikt/ds-icons';
-import { Link } from '@navikt/ds-react';
+import { ExternalLinkIcon, PadlockLockedIcon } from '@navikt/aksel-icons';
+import { BodyShort, HStack, Link } from '@navikt/ds-react';
 import type { IDokumentInfo } from '@navikt/familie-typer';
 
 import { Vedleggsliste, EllipsisBodyShort } from './JournalpostListe';
 import type { FamilieAxiosRequestConfig } from '../../../context/AppContext';
+import type { ITilgangsstyrtJournalpost } from '../../../typer/journalpost';
 
 const ListeElement = styled.li`
     margin-bottom: 1rem;
@@ -16,66 +17,71 @@ const ListeElement = styled.li`
     }
 `;
 
-const DokumentTittelMedLenkeWrapper = styled.div`
-    margin-bottom: 1rem;
-    display: flex;
-    justify-content: flex-start;
-`;
-
-const StyledLink = styled(Link)`
-    margin-left: 0.5rem;
-`;
-
 interface IProps {
     dokument: IDokumentInfo;
-    journalpostId: string;
     hentForhåndsvisning: <D>(familieAxiosRequestConfig: FamilieAxiosRequestConfig<D>) => void;
+    tilgangsstyrtJournalpost: ITilgangsstyrtJournalpost;
 }
 
 export const JournalpostDokument: React.FC<IProps> = ({
     dokument,
-    journalpostId,
     hentForhåndsvisning,
+    tilgangsstyrtJournalpost,
 }) => {
+    const { journalpost, harTilgang } = tilgangsstyrtJournalpost;
+
     const hentPdfDokument = (dokumentId: string | undefined) => {
         if (dokumentId !== undefined) {
             hentForhåndsvisning({
                 method: 'GET',
-                url: `/familie-ks-sak/api/journalpost/${journalpostId}/dokument/${dokumentId}`,
+                url: `/familie-ks-sak/api/journalpost/${journalpost.journalpostId}/dokument/${dokumentId}`,
             });
         } else {
             alert('Klarer ikke å åpne dokument. Ta kontakt med teamet.');
         }
     };
+
+    const dokumentTittel = dokument.tittel || 'Uten tittel';
+
     return (
         <ListeElement>
-            <DokumentTittelMedLenkeWrapper>
-                <EllipsisBodyShort size="small" title={dokument.tittel}>
-                    <Link href="#" onClick={() => hentPdfDokument(dokument.dokumentInfoId)}>
-                        {dokument.tittel}
-                    </Link>
-                </EllipsisBodyShort>
+            <HStack gap="1">
+                {harTilgang ? (
+                    <>
+                        <EllipsisBodyShort size="small" title={dokumentTittel}>
+                            <Link href="#" onClick={() => hentPdfDokument(dokument.dokumentInfoId)}>
+                                {dokumentTittel}
+                            </Link>
+                        </EllipsisBodyShort>
 
-                <StyledLink
-                    href={`/familie-ks-sak/api/journalpost/${journalpostId}/dokument/${dokument.dokumentInfoId}/pdf`}
-                    target="_blank"
-                    aria-label="Åpne dokument i ny fane"
-                    title="Åpne dokument i ny fane"
-                >
-                    <ExternalLink />
-                </StyledLink>
-            </DokumentTittelMedLenkeWrapper>
+                        <Link
+                            href={`/familie-ks-sak/api/journalpost/${journalpost.journalpostId}/dokument/${dokument.dokumentInfoId}/pdf`}
+                            target="_blank"
+                            aria-label="Åpne dokument i ny fane"
+                            title="Åpne dokument i ny fane"
+                        >
+                            <ExternalLinkIcon />
+                        </Link>
+                    </>
+                ) : (
+                    <>
+                        <BodyShort size="small">{dokumentTittel}</BodyShort>
+                        <PadlockLockedIcon title="Dokumentet krever ekstra tilganger" />
+                    </>
+                )}
+            </HStack>
 
-            <Vedleggsliste>
-                {dokument.logiskeVedlegg &&
-                    dokument.logiskeVedlegg.map(vedlegg => (
+            {dokument.logiskeVedlegg && dokument.logiskeVedlegg.length > 0 && (
+                <Vedleggsliste>
+                    {dokument.logiskeVedlegg.map(vedlegg => (
                         <ListeElement key={vedlegg.logiskVedleggId}>
                             <EllipsisBodyShort size="small" title={vedlegg.tittel}>
                                 {vedlegg.tittel}
                             </EllipsisBodyShort>
                         </ListeElement>
                     ))}
-            </Vedleggsliste>
+                </Vedleggsliste>
+            )}
         </ListeElement>
     );
 };
