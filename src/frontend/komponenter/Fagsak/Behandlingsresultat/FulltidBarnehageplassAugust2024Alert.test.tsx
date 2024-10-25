2e699637d7a53b30f8abfa7507780d198441b3c0
thoalm <thoalm@outlook.com>
2024-10-25 14:29:16 +0200

    NAV-22906: La til komponent tester

--- src/frontend/komponenter/Fagsak/Behandlingsresultat/FulltidBarnehageplassAugust2024Alert.test.tsx
+++ src/frontend/komponenter/Fagsak/Behandlingsresultat/FulltidBarnehageplassAugust2024Alert.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
+import { kjønnType } from '@navikt/familie-typer';
 
 import { FulltidBarnehageplassAugust2024Alert } from './FulltidBarnehageplassAugust2024Alert';
-import { type Utbetalingsperiode, Vedtaksperiodetype } from '../../../typer/vedtaksperiode';
+import { type IGrunnlagPerson, PersonType } from '../../../typer/person';
+import { Målform } from '../../../typer/søknad';
+import {
+    type IUtbetalingsperiodeDetalj,
+    type Utbetalingsperiode,
+    Vedtaksperiodetype,
+} from '../../../typer/vedtaksperiode';
 
 describe('FulltidBarnehageplassAugust2024Alert', () => {
     test('skal ikke rendre komponent om ingen utbetalingsperioder har tom i august 2024', () => {
@@ -11,17 +19,65 @@ describe('FulltidBarnehageplassAugust2024Alert', () => {
         render(<FulltidBarnehageplassAugust2024Alert utbetalingsperioder={[]} />);
 
         // Assert
-        // expect(screen.findByRole('Alert')).not.toBeInDocument() TODO : Fix me
+        expect(screen.queryByRole('img', { name: 'Advarsel' })).not.toBeInTheDocument();
+        expect(
+            screen.queryByText(
+                'Det er perioder som kan føre til utbetaling for barn 123456 78901, 123456 78902. ' +
+                    'Kontroller om barnet hører inn under regelverk før lovendring 1. august 24. ' +
+                    'Bruk «Endret utbetalingsperiode» for å stoppe etterbetalingen hvis barnet hadde ' +
+                    'fulltidsplass i barnehage i august 24.'
+            )
+        ).not.toBeInTheDocument();
     });
 
-    test('skal rendre komponent om minst en utbetalingsperiode har tom i august 2024', () => {
+    test('skal rendre komponent hvor utbetalingsperiode har tom i august 2024 for to barn', () => {
         // Arrange
+        const barn1: IGrunnlagPerson = {
+            fødselsdato: '2023-08-01',
+            kjønn: kjønnType.MANN,
+            navn: 'Albus',
+            personIdent: '12345678901',
+            type: PersonType.BARN,
+            målform: Målform.NB,
+        };
+
+        const barn2: IGrunnlagPerson = {
+            fødselsdato: '2023-08-01',
+            kjønn: kjønnType.MANN,
+            navn: 'Harry',
+            personIdent: '12345678902',
+            type: PersonType.BARN,
+            målform: Målform.NB,
+        };
+
+        const utbetalingsperiodeDetaljForBarn1: IUtbetalingsperiodeDetalj = {
+            person: barn1,
+            utbetaltPerMnd: 1,
+            prosent: 50,
+            erPåvirketAvEndring: false,
+        };
+
+        const utbetalingsperiodeDetaljForBarn2: IUtbetalingsperiodeDetalj = {
+            person: barn2,
+            utbetaltPerMnd: 1,
+            prosent: 50,
+            erPåvirketAvEndring: false,
+        };
+
         const utbetalingsperioder: Utbetalingsperiode[] = [
             {
-                periodeFom: '2024-01-01',
+                periodeFom: '2024-05-01',
                 periodeTom: '2024-08-01',
                 vedtaksperiodetype: Vedtaksperiodetype.UTBETALING,
-                utbetalingsperiodeDetaljer: [],
+                utbetalingsperiodeDetaljer: [utbetalingsperiodeDetaljForBarn1],
+                antallBarn: 1,
+                utbetaltPerMnd: 1,
+            },
+            {
+                periodeFom: '2024-05-01',
+                periodeTom: '2024-08-31',
+                vedtaksperiodetype: Vedtaksperiodetype.UTBETALING,
+                utbetalingsperiodeDetaljer: [utbetalingsperiodeDetaljForBarn2],
                 antallBarn: 1,
                 utbetaltPerMnd: 1,
             },
@@ -31,6 +87,82 @@ describe('FulltidBarnehageplassAugust2024Alert', () => {
         render(<FulltidBarnehageplassAugust2024Alert utbetalingsperioder={utbetalingsperioder} />);
 
         // Assert
-        // expect(screen.findByRole('Alert')).toBeInDocument() TODO : Fix me
+        expect(screen.getByRole('img', { name: 'Advarsel' })).toBeInTheDocument();
+        expect(
+            screen.getByText(
+                'Det er perioder som kan føre til utbetaling for barn 123456 78901, 123456 78902. ' +
+                    'Kontroller om barnet hører inn under regelverk før lovendring 1. august 24. ' +
+                    'Bruk «Endret utbetalingsperiode» for å stoppe etterbetalingen hvis barnet hadde ' +
+                    'fulltidsplass i barnehage i august 24.'
+            )
+        ).toBeInTheDocument();
+    });
+
+    test('skal rendre komponent hvor utbetalingsperiode har tom i august 2024 for et barn', () => {
+        // Arrange
+        const barn1: IGrunnlagPerson = {
+            fødselsdato: '2023-08-01',
+            kjønn: kjønnType.MANN,
+            navn: 'Albus',
+            personIdent: '12345678901',
+            type: PersonType.BARN,
+            målform: Målform.NB,
+        };
+
+        const barn2: IGrunnlagPerson = {
+            fødselsdato: '2023-08-01',
+            kjønn: kjønnType.MANN,
+            navn: 'Harry',
+            personIdent: '12345678902',
+            type: PersonType.BARN,
+            målform: Målform.NB,
+        };
+
+        const utbetalingsperiodeDetaljForBarn1: IUtbetalingsperiodeDetalj = {
+            person: barn1,
+            utbetaltPerMnd: 1,
+            prosent: 50,
+            erPåvirketAvEndring: false,
+        };
+
+        const utbetalingsperiodeDetaljForBarn2: IUtbetalingsperiodeDetalj = {
+            person: barn2,
+            utbetaltPerMnd: 1,
+            prosent: 50,
+            erPåvirketAvEndring: false,
+        };
+
+        const utbetalingsperioder: Utbetalingsperiode[] = [
+            {
+                periodeFom: '2024-05-01',
+                periodeTom: '2024-08-01',
+                vedtaksperiodetype: Vedtaksperiodetype.UTBETALING,
+                utbetalingsperiodeDetaljer: [utbetalingsperiodeDetaljForBarn1],
+                antallBarn: 1,
+                utbetaltPerMnd: 1,
+            },
+            {
+                periodeFom: '2024-04-01',
+                periodeTom: '2024-07-31',
+                vedtaksperiodetype: Vedtaksperiodetype.UTBETALING,
+                utbetalingsperiodeDetaljer: [utbetalingsperiodeDetaljForBarn2],
+                antallBarn: 1,
+                utbetaltPerMnd: 1,
+            },
+        ];
+
+        // Act
+        render(<FulltidBarnehageplassAugust2024Alert utbetalingsperioder={utbetalingsperioder} />);
+
+        // Assert
+        expect(screen.getByRole('img', { name: 'Advarsel' })).toBeInTheDocument();
+        expect(
+            screen.getByText(
+                'Det er perioder som kan føre til utbetaling for barn 123456 78901. ' +
+                    'Kontroller om barnet hører inn under regelverk før lovendring 1. august 24. ' +
+                    'Bruk «Endret utbetalingsperiode» for å stoppe etterbetalingen hvis barnet hadde ' +
+                    'fulltidsplass i barnehage i august 24.'
+            )
+        ).toBeInTheDocument();
     });
 });
