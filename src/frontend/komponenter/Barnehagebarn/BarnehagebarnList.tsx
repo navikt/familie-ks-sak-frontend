8bc1b335fdba481e32bc9f2b0449c1da96fc3321
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2025-03-10 10:10:39 +0100

    midtstill antall timer i barnehagelister (#1088)

--- src/frontend/komponenter/Barnehagebarn/BarnehagebarnList.tsx
+++ src/frontend/komponenter/Barnehagebarn/BarnehagebarnList.tsx
@@ -80,7 +80,7 @@ const BarnehagebarnList: React.FunctionComponent = () => {
                                         tilFormat: Datoformat.DATO_FORKORTTET,
                                     })}
                                 </Table.DataCell>
-                                <Table.DataCell align="right">
+                                <Table.DataCell align="center">
                                     {barnehagebarn.antallTimerIBarnehage}
                                 </Table.DataCell>
                                 <Table.DataCell>{barnehagebarn.endringstype}</Table.DataCell>
