af929d38a83988cc5b27fdb65ae4e24f667c679a
Ida Merete Enholm <ida.merete.enholm@nav.no>
2025-03-17 15:43:01 +0100

    fiks mock av klagebehandlinger

--- src/frontend/utils/test/minimalFagsak/minimalFagsak.mock.ts
+++ src/frontend/utils/test/minimalFagsak/minimalFagsak.mock.ts
@@ -37,5 +37,6 @@ export const mockMinimalFagsak = ({
     underBehandling,
     gjeldendeUtbetalingsperioder,
     tilbakekrevingsbehandlinger: [],
+    klagebehandlinger: [],
     løpendeKategori,
 });
