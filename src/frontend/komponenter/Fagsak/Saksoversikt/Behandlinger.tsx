aa45960d313fbe71c253a13cdd0d55557d6856b1
Ida Merete Enholm <ida.merete.enholm@nav.no>
2025-03-24 14:41:25 +0100

    vis warning på saksoversikt hvis klagebehandling-kall har feilet

--- src/frontend/komponenter/Fagsak/Saksoversikt/Behandlinger.tsx
+++ src/frontend/komponenter/Fagsak/Saksoversikt/Behandlinger.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { differenceInMilliseconds } from 'date-fns';
 import styled from 'styled-components';
 
-import { BodyShort, Heading, Switch, Table } from '@navikt/ds-react';
+import { Alert, BodyShort, Heading, Switch, Table, VStack } from '@navikt/ds-react';
 
 import { Behandling } from './Behandling';
 import type { Saksoversiktsbehandling } from './utils';
@@ -16,14 +16,11 @@ import {
 import { useFagsakContext } from '../../../context/fagsak/FagsakContext';
 import type { IMinimalFagsak } from '../../../typer/fagsak';
 import { isoStringTilDate } from '../../../utils/dato';
+import { ressursHarFeilet } from '../../../utils/ressursUtils';
 
 const SwitchHøyre = styled(Switch)`
     margin-right: 0.275rem;
     float: right;
-`;
-
-const StyledHeading = styled(Heading)`
-    margin-top: 3.75rem;
 `;
 
 const StyledOpprettetKolonne = styled(Table.HeaderCell)`
@@ -39,7 +36,7 @@ interface IBehandlingshistorikkProps {
 }
 
 const Behandlinger: React.FC<IBehandlingshistorikkProps> = ({ minimalFagsak }) => {
-    const { klagebehandlinger } = useFagsakContext();
+    const { klagebehandlinger, klageStatus } = useFagsakContext();
 
     const behandlinger = hentBehandlingerTilSaksoversikten(minimalFagsak, klagebehandlinger);
 
@@ -50,58 +47,67 @@ const Behandlinger: React.FC<IBehandlingshistorikkProps> = ({ minimalFagsak }) =
     const [visHenlagteBehandlinger, setVisHenlagteBehandlinger] = useState(false);
 
     return (
-        <>
-            <StyledHeading level="2" size={'medium'} spacing>
-                Behandlinger
-                {finnesRadSomKanFiltreresBort && (
-                    <SwitchHøyre
-                        size="small"
-                        position="left"
-                        id={'vis-henlagte-behandlinger'}
-                        checked={visHenlagteBehandlinger}
-                        onChange={() => {
-                            setVisHenlagteBehandlinger(!visHenlagteBehandlinger);
-                        }}
-                    >
-                        Vis henlagte behandlinger
-                    </SwitchHøyre>
-                )}
-            </StyledHeading>
-            {behandlinger.length > 0 ? (
-                <Table size={'large'}>
-                    <Table.Header>
-                        <Table.Row>
-                            <StyledOpprettetKolonne children={'Opprettet'} />
-                            <Table.HeaderCell children={'Årsak'} />
-                            <Table.HeaderCell children={'Type'} />
-                            <Table.HeaderCell children={'Behandlingstema'} />
-                            <Table.HeaderCell children={'Status'} />
-                            <Table.HeaderCell children={'Vedtaksdato'} />
-                            <StyledResultatKolonne children={'Resultat'} />
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {behandlinger
-                            .filter(behandling => skalRadVises(behandling, visHenlagteBehandlinger))
-                            .sort((a, b) =>
-                                differenceInMilliseconds(
-                                    isoStringTilDate(hentTidspunktForSortering(b)),
-                                    isoStringTilDate(hentTidspunktForSortering(a))
-                                )
-                            )
-                            .map((behandling: Saksoversiktsbehandling) => (
-                                <Behandling
-                                    key={hentBehandlingId(behandling)}
-                                    saksoversiktsbehandling={behandling}
-                                    minimalFagsak={minimalFagsak}
-                                />
-                            ))}
-                    </Table.Body>
-                </Table>
-            ) : (
-                <BodyShort children={'Ingen tidligere behandlinger'} />
+        <VStack gap="6">
+            {ressursHarFeilet(klageStatus) && (
+                <Alert variant="warning">
+                    <BodyShort>Klagebehandlinger er ikke tilgjengelig for øyeblikket.</BodyShort>
+                </Alert>
             )}
-        </>
+            <div>
+                <Heading level="2" size={'medium'} spacing>
+                    Behandlinger
+                    {finnesRadSomKanFiltreresBort && (
+                        <SwitchHøyre
+                            size="small"
+                            position="left"
+                            id={'vis-henlagte-behandlinger'}
+                            checked={visHenlagteBehandlinger}
+                            onChange={() => {
+                                setVisHenlagteBehandlinger(!visHenlagteBehandlinger);
+                            }}
+                        >
+                            Vis henlagte behandlinger
+                        </SwitchHøyre>
+                    )}
+                </Heading>
+                {behandlinger.length > 0 ? (
+                    <Table size={'large'}>
+                        <Table.Header>
+                            <Table.Row>
+                                <StyledOpprettetKolonne children={'Opprettet'} />
+                                <Table.HeaderCell children={'Årsak'} />
+                                <Table.HeaderCell children={'Type'} />
+                                <Table.HeaderCell children={'Behandlingstema'} />
+                                <Table.HeaderCell children={'Status'} />
+                                <Table.HeaderCell children={'Vedtaksdato'} />
+                                <StyledResultatKolonne children={'Resultat'} />
+                            </Table.Row>
+                        </Table.Header>
+                        <Table.Body>
+                            {behandlinger
+                                .filter(behandling =>
+                                    skalRadVises(behandling, visHenlagteBehandlinger)
+                                )
+                                .sort((a, b) =>
+                                    differenceInMilliseconds(
+                                        isoStringTilDate(hentTidspunktForSortering(b)),
+                                        isoStringTilDate(hentTidspunktForSortering(a))
+                                    )
+                                )
+                                .map((behandling: Saksoversiktsbehandling) => (
+                                    <Behandling
+                                        key={hentBehandlingId(behandling)}
+                                        saksoversiktsbehandling={behandling}
+                                        minimalFagsak={minimalFagsak}
+                                    />
+                                ))}
+                        </Table.Body>
+                    </Table>
+                ) : (
+                    <BodyShort children={'Ingen tidligere behandlinger'} />
+                )}
+            </div>
+        </VStack>
     );
 };
 
