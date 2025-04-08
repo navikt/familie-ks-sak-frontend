dba9684eafd3db867650ac052fc759dad6bad5ae
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2025-04-08 13:23:15 +0200

    flytt informasjonsbolk inn til samme mappe som eneste komponent som bruker den

--- src/frontend/sider/Fagsak/Behandling/Høyremeny/Behandlingskort.tsx
+++ src/frontend/sider/Fagsak/Behandling/Høyremeny/Behandlingskort.tsx
@@ -16,8 +16,8 @@ import {
 } from '@navikt/ds-tokens/dist/tokens';
 import { hentDataFraRessurs } from '@navikt/familie-typer';
 
+import Informasjonsbolk from './Informasjonsbolk';
 import { useFagsakContext } from '../../../../context/fagsak/FagsakContext';
-import Informasjonsbolk from '../../../../komponenter/Informasjonsbolk/Informasjonsbolk';
 import type { IBehandling } from '../../../../typer/behandling';
 import {
     BehandlingResultat,
