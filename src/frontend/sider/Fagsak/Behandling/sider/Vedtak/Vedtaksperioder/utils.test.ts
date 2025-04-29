e85a0a59520b97c23ca47f5b0ccb6160e3af4bd3
Hanne Olsen <hanne.olsen@nav.no>
2025-04-29 10:39:27 +0200

    Rename testfil

--- src/frontend/sider/Fagsak/Behandling/sider/Vedtak/Vedtaksperioder/utils.test.ts
+++ src/frontend/sider/Fagsak/Behandling/sider/Vedtak/Vedtaksperioder/utils.test.ts
@@ -13,7 +13,7 @@ import {
     mockUtbetalingsperiode,
 } from '../../../../../../utils/test/vedtak/vedtaksperiode.mock';
 
-describe('VedtaksBegrunnelserContext', () => {
+describe('Vedtak utils', () => {
     describe('Test filtrerOgSorterPerioderMedBegrunnelseBehov', () => {
         const fom = '2020-01-01';
         const tom = '2020-02-28';
