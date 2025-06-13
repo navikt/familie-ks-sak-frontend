25bdb640f77e3fc9d498ab86e6c1524be150b217
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2025-06-13 12:21:08 +0200

    Legger til visning av lenke og ulike states

--- src/frontend/sider/Fagsak/Personlinje/Behandlingsmeny/AInntekt/AInntekt.tsx
+++ src/frontend/sider/Fagsak/Personlinje/Behandlingsmeny/AInntekt/AInntekt.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 
-import { Dropdown } from '@navikt/ds-react';
+import { Alert, Dropdown, Loader } from '@navikt/ds-react';
 import { useHttp } from '@navikt/familie-http';
+import { byggHenterRessurs, RessursStatus, type Ressurs } from '@navikt/familie-typer';
 
 import type { IMinimalFagsak } from '../../../../../typer/fagsak';
 
@@ -12,19 +13,44 @@ interface IProps {
 export const AInntekt: React.FC<IProps> = ({ minimalFagsak }) => {
     const { request } = useHttp();
 
+    const [aInntektUrlRessurs, settAInntektUrlRessurs] =
+        React.useState<Ressurs<string>>(byggHenterRessurs());
+
     useEffect(() => {
-        request({
+        request<{ ident: string }, string>({
             method: 'POST',
             url: 'familie-ks-sak/api/a-inntekt/hent-url',
             data: {
                 ident: minimalFagsak.søkerFødselsnummer,
             },
-        })
-            .then(response => {
-                console.log('A-inntekt URL:', response);
-            })
-            .catch(_ => {});
+        }).then(response => {
+            settAInntektUrlRessurs(response);
+        });
     }, []);
 
-    return <Dropdown.Menu.List.Item>A-Inntekt</Dropdown.Menu.List.Item>;
+    return (
+        <>
+            {aInntektUrlRessurs.status === RessursStatus.SUKSESS && (
+                <Dropdown.Menu.List.Item
+                    onClick={() => window.open(aInntektUrlRessurs.data, '_blank')}
+                >
+                    A-Inntekt
+                </Dropdown.Menu.List.Item>
+            )}
+
+            {aInntektUrlRessurs.status === RessursStatus.HENTER && (
+                <Dropdown.Menu.List.Item disabled>
+                    Henter A-Inntekt <Loader size="xsmall" />
+                </Dropdown.Menu.List.Item>
+            )}
+
+            {aInntektUrlRessurs.status !== RessursStatus.FEILET && (
+                <Dropdown.Menu.List.Item disabled>
+                    <Alert variant="error" inline>
+                        A-Inntekt er ikke tilgjengelig
+                    </Alert>
+                </Dropdown.Menu.List.Item>
+            )}
+        </>
+    );
 };
