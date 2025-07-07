447c913775407024d954e31979b7652b8de2ae5c
Uy Nguyen <uy.quang.nguyen@nav.no>
2025-07-07 08:49:07 +0200

    Legg til toggle

--- src/frontend/typer/toggles.ts
+++ src/frontend/typer/toggles.ts
@@ -10,6 +10,7 @@ export enum ToggleNavn {
     skalObfuskereData = 'familie-ks-sak.anonymiser-persondata',
     kanOppretteRevurderingMedAarsakIverksetteKaVedtak = 'familie-ks-sak.kan-opprette-revurdering-med-aarsak-iverksette-ka-vedtak',
     skalAlltidViseAlleVedtaksperioder = 'familie-ks-sak-frontend.alltid-vis-alle-vedtaksperioder',
+    bosattSvalbard = 'familie-ks-sak.bosatt-svalbard',
 }
 
 export const alleTogglerAv = (): IToggles => {
