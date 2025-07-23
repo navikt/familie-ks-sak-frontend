5e4f2f9ef42b2619c68b6251cc3a843cbad28cd5
NghiNg <60237320+NghiNg@users.noreply.github.com>
2025-07-23 14:08:29 +0200

    Sentrer spinner og tekst ved lasting av pdf dokument (#1249)

--- src/frontend/komponenter/PdfVisningModal/PdfVisningModal.tsx
+++ src/frontend/komponenter/PdfVisningModal/PdfVisningModal.tsx
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 
 import styled from 'styled-components';
 
-import { Alert, Heading, Loader, Modal } from '@navikt/ds-react';
-import { RessursStatus } from '@navikt/familie-typer';
+import { Alert, Heading, HStack, Loader, Modal, VStack } from '@navikt/ds-react';
 import type { Ressurs } from '@navikt/familie-typer';
+import { RessursStatus } from '@navikt/familie-typer';
 
 interface IPdfVisningModalProps {
     onRequestClose: () => void;
@@ -58,10 +58,12 @@ const Dokument: React.FC<{ pdfdata: Ressurs<string> }> = ({ pdfdata }) => {
     switch (pdfdata.status) {
         case RessursStatus.HENTER:
             return (
-                <div className={'pdfvisning-modal__spinner'}>
-                    <Heading size={'small'} level={'2'} children={'Innhenter dokument'} />
-                    <Loader size="xlarge" title="Innhenter dokument" />
-                </div>
+                <HStack justify={'center'} height={'100%'} align={'center'}>
+                    <VStack align={'center'}>
+                        <Heading size={'small'} level={'2'} children={'Innhenter dokument'} />
+                        <Loader size="xlarge" title="Innhenter dokument" />
+                    </VStack>
+                </HStack>
             );
         case RessursStatus.SUKSESS:
             return <IframePdfVisning title={'Dokument'} src={pdfdata.data} tabIndex={0} />;
