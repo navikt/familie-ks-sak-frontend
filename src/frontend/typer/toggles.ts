247f610609242af0b19cf6af840ef4f7d39bcdb0
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2025-09-24 15:26:45 +0200

    NAV-26322: Fjerner toggle for å ta i bruk ny henlegg behandling modal (#1365)

--- src/frontend/typer/toggles.ts
+++ src/frontend/typer/toggles.ts
@@ -10,7 +10,6 @@ export enum ToggleNavn {
     skalObfuskereData = 'familie-ks-sak.anonymiser-persondata',
     kanOppretteRevurderingMedAarsakIverksetteKaVedtak = 'familie-ks-sak.kan-opprette-revurdering-med-aarsak-iverksette-ka-vedtak',
     skalAlltidViseAlleVedtaksperioder = 'familie-ks-sak-frontend.alltid-vis-alle-vedtaksperioder',
-    brukNyHenleggModal = 'familie-ks-sak.bruk-ny-henlegg-modal',
     brukReactQueryPaaSaksoversiktsiden = 'familie-ks-sak.bruk-react-query-paa-saksoversiktsiden',
     skalViseOppholdsadresse = 'familie-ks-sak.skal-vise-oppholdsadresse',
     brukNyPdfModal = 'familie-ks-sak.bruk-ny-pdf-modal',
