d22e1f0e0f366dbb312603e54311dca07ad55a11
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-09-29 19:09:35 +0200

    wip 15 - sentry

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,9 +1,33 @@
+import console from 'console';
+import process from 'process';
+
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        // Sentry må være siste plugin
+        sentryVitePlugin({
+            org: 'nav',
+            project: 'familie-ks-sak-frontend',
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+            url: 'https://sentry.gc.nav.no/',
+            release: {
+                name: process.env.SENTRY_RELEASE,
+                uploadLegacySourcemaps: {
+                    paths: ['./frontend'],
+                    ignore: ['./node_modules'],
+                    urlPrefix: `~/assets`,
+                },
+            },
+            errorHandler: err => {
+                console.warn('Sentry CLI Plugin: ' + err.message);
+            },
+        }),
+    ],
     server: {
         port: 8000,
     },
