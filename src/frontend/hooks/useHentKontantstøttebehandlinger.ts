7cb2c854bbffea7235c1fd82da29991c165d3f56
NghiNg <60237320+NghiNg@users.noreply.github.com>
2025-09-29 15:34:03 +0200

    oppdater opprett behandling funksjoner til å invalidate hente queries (#1364)
    * oppdater opprett behandling funksjoner til å invalidate hente queries
    
    * Rydder opp i kode
    
    ---------
    
    Co-authored-by: thoalm <thoalm@outlook.com>

--- src/frontend/hooks/useHentKontantstøttebehandlinger.ts
+++ src/frontend/hooks/useHentKontantstøttebehandlinger.ts
@@ -4,14 +4,15 @@ import { useHttp } from '@navikt/familie-http';
 
 import { hentKontantstøtteBehandlinger } from '../api/hentKontantstøtteBehandlinger';
 
-export const HentBehandlingerQueryKeyFactory = {
-    fagsak: (fagsakId: number | undefined) => ['visningsBehandlinger', fagsakId],
+export const HentKontantstøttebehandlingerQueryKeyFactory = {
+    // TODO : Fjern "undefined" når FagsakContext alltid inneholder en fagsak
+    kontantstøttebehandlinger: (fagsakId: number | undefined) => ['kontantstøttebehandlinger', fagsakId],
 };
 
-export function useHentKontantstøtteBehandlinger(fagsakId: number | undefined, påvirkerSystemLaster: boolean = true) {
+export function useHentKontantstøttebehandlinger(fagsakId: number | undefined, påvirkerSystemLaster: boolean = true) {
     const { request } = useHttp();
     return useQuery({
-        queryKey: HentBehandlingerQueryKeyFactory.fagsak(fagsakId),
+        queryKey: HentKontantstøttebehandlingerQueryKeyFactory.kontantstøttebehandlinger(fagsakId),
         queryFn: () => {
             if (fagsakId === undefined) {
                 return Promise.reject(new Error('Kan ikke hente fagsak uten fagsakId.'));
