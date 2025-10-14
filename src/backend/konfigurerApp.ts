1d26e99d5a50d576c449bb52538e80069c72462e
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-10-14 11:22:26 +0200

    Pakk konfigurering i en funksjon
    Legg tilbake original logging

--- src/backend/konfigurerApp.ts
+++ src/backend/konfigurerApp.ts
@@ -1,18 +1,21 @@
 import dotenvx from '@dotenvx/dotenvx';
 
+import { logInfo } from '@navikt/familie-logging';
+
 import { envVar } from './env.js';
 
-dotenvx.config({
-    path: ['.env', '.secrets.env', `.env.${envVar('ENV')}`],
-    ignore: ['MISSING_ENV_FILE'],
-});
+const konfigurerApp = () => {
+    logInfo(`NODE_ENV=${process.env.NODE_ENV} ENV=${process.env.ENV}, CLUSTER=${process.env.CLUSTER}`);
 
-// felles-backend bruker andre variabler enn det som blir satt opp av azureAd
-export const remapEnvForFamilieBackend = () => {
-    // TODO: Remapping for familie-backend
+    dotenvx.config({
+        path: ['.env', '.secrets.env', `.env.${envVar('ENV')}`],
+        ignore: ['MISSING_ENV_FILE'],
+    });
+
+    // felles-backend bruker andre variabler enn det som blir satt opp av azureAd
     process.env.AAD_DISCOVERY_URL = envVar('AZURE_APP_WELL_KNOWN_URL');
     process.env.CLIENT_ID = envVar('AZURE_APP_CLIENT_ID');
     process.env.CLIENT_SECRET = envVar('AZURE_APP_CLIENT_SECRET');
 };
 
-remapEnvForFamilieBackend();
+konfigurerApp();
