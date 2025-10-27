e1638f1ed0e50046f0e9b88cab9e853752a95eac
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-10-27 15:47:14 +0100

    Send inn appConfig til familie-backend
    Litt rydding i config i samme slengen :)

--- src/backend/konfigurerApp.ts
+++ src/backend/konfigurerApp.ts
@@ -11,11 +11,6 @@ const konfigurerApp = () => {
         path: ['.env', '.secrets.env', `.env.${envVar('ENV')}`],
         ignore: ['MISSING_ENV_FILE'],
     });
-
-    // felles-backend bruker andre variabler enn det som blir satt opp av azureAd
-    process.env.AAD_DISCOVERY_URL = envVar('AZURE_APP_WELL_KNOWN_URL');
-    process.env.CLIENT_ID = envVar('AZURE_APP_CLIENT_ID');
-    process.env.CLIENT_SECRET = envVar('AZURE_APP_CLIENT_SECRET');
 };
 
 konfigurerApp();
