5e3bf6612d1ac23a29c15a79b8ed42585152dbc0
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2025-10-28 12:43:00 +0100

    legg til spinner for når vi henter barnehagebarn in bakgrunnen

--- src/frontend/sider/Barnehagelister/BarnehagelisterInnhold.tsx
+++ src/frontend/sider/Barnehagelister/BarnehagelisterInnhold.tsx
@@ -5,6 +5,7 @@ import { Heading } from '@navikt/ds-react';
 import { BarnehagebarnTabell } from './BarnehagebarnTabell';
 import BarnehagebarnTabellNavigator from './BarnehagebarnTabellNavigator';
 import BarnehagelisterFilterskjema from './BarnehagelisterFilterskjema';
+import { LasterNyttTabellInnholdSpinner } from './LasterNyttTabellInnholdSpinner';
 import { useBarnehagelister } from './useBarnehagelister';
 
 const Container = styled.div`
@@ -24,6 +25,9 @@ const BarnehagelisterInnhold = () => {
             </Heading>
             <BarnehagelisterFilterskjema {...barnehagelisterContext} />
             <BarnehagebarnTabellNavigator {...barnehagelisterContext} />
+            <LasterNyttTabellInnholdSpinner
+                barnehagebarnRequestParams={barnehagelisterContext.barnehagebarnRequestParams}
+            />
             <BarnehagebarnTabell {...barnehagelisterContext} />
         </Container>
     );
