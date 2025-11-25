f97578fddda89158230824aad94713bb1c342ca3
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2025-11-25 13:28:31 +0100

    NAV-26760: Legger til toggle og splitter fagsaklinje opp i en komponent for fagsak og en komponent for behandling (#1481)

---
+++ src/frontend/sider/Fagsak/Fagsaklinje/Behandlingslinje.tsx
@@ -0,0 +1,50 @@
+import { Link as ReactRouterLink, useLocation } from 'react-router';
+
+import { FileTextIcon, HouseIcon } from '@navikt/aksel-icons';
+import { Box, Button, HStack } from '@navikt/ds-react';
+
+import { useAppContext } from '../../../context/AppContext';
+import { useFagsakContext } from '../FagsakContext';
+import { BehandlingsmenyNy } from './Behandlingsmeny/BehandlingsmenyNy';
+
+function lagAktivFaneStyle(fanenavn: string, pathname: string) {
+    const urlSplit = pathname.split('/');
+    const sluttenPåUrl = urlSplit[urlSplit.length - 1];
+    return sluttenPåUrl === fanenavn ? { textDecoration: 'underline' } : {};
+}
+
+export function Behandlingslinje() {
+    const { harInnloggetSaksbehandlerSkrivetilgang } = useAppContext();
+    const { fagsak } = useFagsakContext();
+    const { pathname } = useLocation();
+
+    return (
+        <Box borderWidth={'0 0 1 0'} borderColor={'border-subtle'}>
+            <HStack paddingInline={'2 4'} paddingBlock={'2'} justify={'space-between'}>
+                <HStack>
+                    <Button
+                        as={ReactRouterLink}
+                        size={'small'}
+                        variant={'tertiary'}
+                        icon={<HouseIcon />}
+                        to={`/fagsak/${fagsak.id}/saksoversikt`}
+                        style={lagAktivFaneStyle('saksoversikt', pathname)}
+                    >
+                        Saksoversikt
+                    </Button>
+                    <Button
+                        as={ReactRouterLink}
+                        size={'small'}
+                        variant={'tertiary'}
+                        icon={<FileTextIcon />}
+                        to={`/fagsak/${fagsak.id}/dokumenter`}
+                        style={lagAktivFaneStyle('dokumenter', pathname)}
+                    >
+                        Dokumenter
+                    </Button>
+                </HStack>
+                {harInnloggetSaksbehandlerSkrivetilgang() && <BehandlingsmenyNy />}
+            </HStack>
+        </Box>
+    );
+}
