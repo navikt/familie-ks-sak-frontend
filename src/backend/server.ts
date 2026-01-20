67046988aae25a7a1b0f7750d4ea6b47d989e32c
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2026-01-20 11:40:00 +0100

    NAV-27285: Fjerner body-parser (#1568)

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -2,7 +2,6 @@ import './konfigurerApp.js'; // Må importeres først
 
 import path from 'path';
 
-import bodyParser from 'body-parser';
 import express, { type NextFunction, type Request, type Response } from 'express';
 import expressStaticGzip from 'express-static-gzip';
 import { v4 as uuidv4 } from 'uuid';
@@ -34,9 +33,8 @@ backend(sessionConfig, prometheusTellere, appConfig).then(async ({ app, azureAut
 
     app.use('/redirect', doRedirectProxy());
 
-    // Sett opp bodyParser og router etter proxy. Spesielt viktig med tanke på større payloads som blir parset av bodyParser
-    app.use(bodyParser.json({ limit: '200mb' }));
-    app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
+    app.use(express.json({ limit: '200mb' }));
+    app.use(express.urlencoded({ limit: '200mb', extended: true }));
     app.use('/', await setupRouter(azureAuthClient, router));
 
     app.listen(port, '0.0.0.0', () => {
