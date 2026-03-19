adedf571e9c52a99175e6d0b085df9304d4b16b9
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2026-03-19 12:45:40 +0100

    Optimalisering av splat-route

--- src/backend/router.ts
+++ src/backend/router.ts
@@ -51,6 +51,8 @@ export default async (authClient: Client, router: Router) => {
         router.use(vite.middlewares);
     }
 
+    const htmlPath = path.join(process.cwd(), frontendPath, 'index.html');
+
     // APP
     router.get(
         '*splat',
@@ -59,14 +61,13 @@ export default async (authClient: Client, router: Router) => {
         async (req: Request, res: Response) => {
             prometheusTellere.appLoad.inc();
 
-            const htmlPath = path.join(process.cwd(), frontendPath, 'index.html');
-            let htmlInnhold = fs.readFileSync(htmlPath, 'utf-8');
-
             if (erLokal()) {
-                htmlInnhold = await vite.transformIndexHtml(req.url, htmlInnhold);
+                const htmlInnhold = await fs.promises.readFile(htmlPath, 'utf-8');
+                const transformed = await vite.transformIndexHtml(req.url, htmlInnhold);
+                res.status(200).type('html').send(transformed);
+            } else {
+                res.sendFile(htmlPath);
             }
-
-            res.status(200).type('html').send(htmlInnhold);
         }
     );
 
