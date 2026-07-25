2c29c9f8226596f10e7b9d2a49559e57fa68655b
Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
2026-07-25 12:21:19 +0200

    NAV-29957: Skriver om valutakurs til react query og rhf (#1903)
    * NAV-29957: Skriver om valutakurs til react query og rhf
    
    * Endringer etter QA

---
+++ src/frontend/sider/Fagsak/Behandling/sider/Behandlingsresultat/Eøs/Valutakurs/ValutakursPeriodeFelt.tsx
@@ -0,0 +1,103 @@
+import MånedÅrVelger from '@komponenter/MånedÅrInput/MånedÅrVelger';
+import { hentDagensDato, isoStringTilDate } from '@utils/dato';
+import { isEmpty } from '@utils/eøsValidators';
+import { addMonths, endOfMonth, isAfter } from 'date-fns';
+import { useController, useFormContext } from 'react-hook-form';
+
+import { Fieldset, HStack } from '@navikt/ds-react';
+
+import { ValutakursFelt, type ValutakursFormValues } from './useValutakursSkjema';
+
+const valgtDatoErNesteMånedEllerSenere = (valgtDato: string) =>
+    isAfter(isoStringTilDate(valgtDato), endOfMonth(hentDagensDato()));
+
+const valgtDatoErSenereEnnNesteMåned = (valgtDato: string) =>
+    isAfter(isoStringTilDate(valgtDato), endOfMonth(addMonths(hentDagensDato(), 1)));
+
+interface Props {
+    initiellFom: string;
+    periodeFeilmeldingId: string;
+    lesevisning: boolean;
+    behandlingsÅrsakErOvergangsordning: boolean;
+}
+
+export function ValutakursPeriodeFelt({
+    initiellFom,
+    periodeFeilmeldingId,
+    lesevisning,
+    behandlingsÅrsakErOvergangsordning,
+}: Props) {
+    const { control } = useFormContext<ValutakursFormValues>();
+
+    const {
+        field: { value, onChange },
+        fieldState: { error },
+    } = useController({
+        name: ValutakursFelt.PERIODE,
+        control,
+        rules: {
+            validate: periode => {
+                const fom = periode.fom;
+                const tom = periode.tom;
+
+                if (!fom || isEmpty(fom)) {
+                    return 'Fra og med måned må være utfylt';
+                }
+                if (fom && valgtDatoErSenereEnnNesteMåned(fom) && !behandlingsÅrsakErOvergangsordning) {
+                    return 'Du kan ikke sette fra og med (f.o.m.) til måneden etter neste måned eller senere';
+                }
+                if (initiellFom && !isAfter(isoStringTilDate(fom), isoStringTilDate(initiellFom))) {
+                    return `Du kan ikke legge inn fra og med måned som er før: ${initiellFom}`;
+                }
+                if (tom && valgtDatoErNesteMånedEllerSenere(tom) && !behandlingsÅrsakErOvergangsordning) {
+                    return 'Du kan ikke sette til og med (t.o.m.) til neste måned eller senere';
+                }
+                return undefined;
+            },
+        },
+    });
+
+    const finnÅrTilbakeTil = (): number => new Date().getFullYear() - new Date(initiellFom).getFullYear();
+    const antallÅrFrem = behandlingsÅrsakErOvergangsordning ? 1 : 0;
+
+    return (
+        <Fieldset
+            className={lesevisning ? 'lesevisning' : ''}
+            errorId={periodeFeilmeldingId}
+            error={error?.message}
+            legend="Periode"
+            size="medium"
+        >
+            <HStack justify={'space-between'} gap={'space-16'} width={'32rem'}>
+                <MånedÅrVelger
+                    lesevisning={lesevisning}
+                    id={'periode_fom'}
+                    label={'F.o.m'}
+                    antallÅrTilbake={finnÅrTilbakeTil()}
+                    antallÅrFrem={antallÅrFrem}
+                    value={value.fom ?? undefined}
+                    onEndret={årMåned => {
+                        if (årMåned === value.fom) {
+                            return;
+                        }
+                        onChange({ ...value, fom: årMåned });
+                    }}
+                />
+                <MånedÅrVelger
+                    lesevisning={lesevisning}
+                    id={'periode_tom'}
+                    label={'T.o.m (valgfri)'}
+                    antallÅrTilbake={finnÅrTilbakeTil()}
+                    antallÅrFrem={antallÅrFrem}
+                    value={value.tom ?? undefined}
+                    onEndret={årMåned => {
+                        if (årMåned === value.tom) {
+                            return;
+                        }
+                        onChange({ ...value, tom: årMåned });
+                    }}
+                />
+            </HStack>
+        </Fieldset>
+    );
+}
